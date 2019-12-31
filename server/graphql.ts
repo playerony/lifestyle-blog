f6e1f8fc8a9b64a315e02397f90a9464d02704e3
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2019-12-31 14:36:28 +0100

    Create User model

--- server/graphql.ts
+++ server/graphql.ts
@@ -3,9 +3,13 @@ import { Container } from '@decorators/di'
 import { ApolloServer } from 'apollo-server-express'
 
 import Role from './database/model/Role'
+import User from './database/model/User'
+import UserRole from './database/model/UserRole'
 
 export default async () => {
   await Role.sync()
+  await User.sync()
+  await UserRole.sync()
 
   const schema = await buildSchema({
     resolvers: [],
