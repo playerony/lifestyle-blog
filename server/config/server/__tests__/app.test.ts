9a30331c13c1bf3bdc00beac0fd9bf0944020c0b
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-01-12 19:25:23 +0100

    Update tests for server

--- server/config/server/__tests__/app.test.ts
+++ server/config/server/__tests__/app.test.ts
@@ -1,45 +1,25 @@
 import { Express } from 'express'
+import waitForExpect from 'wait-for-expect'
 
-jest.mock('sequelize', () => {
-  const { dataTypes: DataTypes } = require('sequelize-test-helpers')
-
-  class Sequelize {
-    public authenticate = jest.fn()
-    public sync = jest.fn()
+jest.mock('../apolloServer', () => {
+  class Server {
+    public applyMiddleware: Function = jest.fn()
   }
 
-  class Model {
-    public static init = jest.fn()
-  }
-
-  return {
-    Model,
-    DataTypes,
-    Sequelize
-  }
+  return jest.fn().mockImplementation(() => new Server())
 })
+
+import apolloServer from '../apolloServer'
 
 describe('app Configuration', () => {
   const OLD_ENV = process.env
   let appConfig: () => Express
-
-  beforeAll(() => {
-    console.warn = jest.fn()
-    console.info = jest.fn()
-  })
 
   beforeEach(() => {
     jest.resetModules()
     process.env = { ...OLD_ENV }
     delete process.env.NODE_ENV
 
-    process.env.DATABASE_NAME = 'database_name'
-    process.env.DATABASE_USER = 'database_user'
-    process.env.DATABASE_PASSWORD = 'database_password'
-    process.env.DATABASE_SERVER = 'database_server'
-    process.env.DATABASE_PORT = '3100'
-    process.env.HASH_SALT = '12'
-    process.env.APP_SECRET = 'secret_key'
     process.env.APP_PORT = '3200'
 
     appConfig = require('../app').default
@@ -49,39 +29,18 @@ describe('app Configuration', () => {
     process.env = OLD_ENV
   })
 
-  it('should throw an error if enviroment variables are not defined', async () => {
-    process.env.APP_SECRET = undefined
-
-    appConfig = require('../app').default
-
-    try {
-      await appConfig()
-    } catch (error) {
-      expect(error.message).toEqual(
-        'Not all environment variables have been defined.'
-      )
-    }
-  })
-
   it('should set properly app port', async () => {
     const result = await appConfig()
 
     expect(result.locals.settings.port).toEqual('3200')
   })
 
-  describe('database configuration', () => {
-    it('should authenticate connection with database', async () => {
-      await appConfig()
-      const { sequelize } = require('../../sequelize')
+  it('should prepare server configuration', async () => {
+    appConfig()
+    await apolloServer()
 
-      expect(sequelize.authenticate).toBeCalled()
-    })
-
-    it('should synchronize models with database', async () => {
-      await appConfig()
-      const { sequelize } = require('../../sequelize')
-
-      expect(sequelize.sync).toBeCalled()
+    await waitForExpect(() => {
+      expect(apolloServer).toHaveBeenCalled()
     })
   })
 })
---
+++ server/config/server/__tests__/server.test.ts
@@ -0,0 +1,22 @@
+jest.mock('../app', () => {
+  class App {
+    public listen = jest.fn()
+    public get = jest.fn()
+  }
+
+  return jest.fn().mockImplementation(() => new App())
+})
+
+import waitForExpect from 'wait-for-expect'
+
+import initializeApp from '../app'
+
+describe('server Configuration', () => {
+  it('should called an app configuration file', async () => {
+    require('../index').default()
+
+    await waitForExpect(() => {
+      expect(initializeApp).toHaveBeenCalled()
+    })
+  })
+})
