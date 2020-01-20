7c3d29075b751a158e2792ad0cd8d5978797ff07
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-01-20 21:04:56 +0100

    Routing tests

--- admin/service/__tests__/Routing.test.tsx
+++ admin/service/__tests__/Routing.test.tsx
@@ -1,14 +1,56 @@
 import React from 'react'
 import { mount } from 'enzyme'
+import { MemoryRouter } from 'react-router';
 
-import LoginPage from '@page/LoginPage'
+import routeList from '@config/routeList'
 
-import Routing from '../Routing'
+jest.doMock('../../page/LoginPage', () => LoginPageMock)
+jest.mock('react-router-dom', () => {
+  const originalModule = jest.requireActual('react-router-dom');
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    BrowserRouter: ({ children }: { children: React.ReactChild }) => <div>{children}</div>
+  };
+});
 
 describe('Routing Service', () => {
   it('should render LoginPage as default', () => {
-    const wrapper = mount(<Routing />)
+    const Routing = require('../Routing').default
 
-    expect(wrapper.find(LoginPage).length).toEqual(1)
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[routeList.baseUrl]}>
+        <Routing />
+      </MemoryRouter>
+    )
+
+    expect(wrapper.find(LoginPageMock)).toHaveLength(1)
+  })
+
+  it('should render any route', () => {
+    const Routing = require('../Routing').default
+
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/test']}>
+        <Routing />
+      </MemoryRouter>
+    )
+
+    expect(wrapper.find('div')).toHaveLength(1)
+  })
+
+  it('should render LoginPage', () => {
+    const Routing = require('../Routing').default
+
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[routeList.loginPageUrl]}>
+        <Routing />
+      </MemoryRouter>
+    )
+
+    expect(wrapper.find(LoginPageMock)).toHaveLength(1)
   })
 })
+
+const LoginPageMock = (): JSX.Element => <div>Login Page</div>
