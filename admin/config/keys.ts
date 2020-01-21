124beaa1da101a02aae8986c4d883924f3376773
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-01-21 21:41:34 +0100

    Hash every value saved in cookie memory

--- admin/config/keys.ts
+++ admin/config/keys.ts
@@ -1,11 +1,13 @@
 interface IKeys {
   nodeEnv?: string
   serverUrl?: string
+  cryptrKey?: string
 }
 
 const config: IKeys = {
   nodeEnv: process.env.NODE_ENV || 'development',
-  serverUrl: process.env.SERVER_URL
+  serverUrl: process.env.SERVER_URL,
+  cryptrKey: process.env.CRYPTR_KEY
 }
 
 export default config
