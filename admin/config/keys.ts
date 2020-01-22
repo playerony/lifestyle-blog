3ac794d0049c5c24ade7cd42d7884ef46aa1b301
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-01-22 18:03:32 +0100

    Add JWT prefix enviroment

--- admin/config/keys.ts
+++ admin/config/keys.ts
@@ -2,12 +2,14 @@ interface IKeys {
   nodeEnv?: string
   serverUrl?: string
   cryptoKey?: string
+  jwtPrefix?: string
 }
 
 const config: IKeys = {
   nodeEnv: process.env.NODE_ENV || 'development',
   serverUrl: process.env.SERVER_URL,
-  cryptoKey: process.env.CRYPTO_KEY
+  cryptoKey: process.env.CRYPTO_KEY,
+  jwtPrefix: process.env.JWT_PREFIX
 }
 
 export default config
