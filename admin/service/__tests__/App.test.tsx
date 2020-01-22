b056a80944d5b354427468fe3504d5c26a05c40c
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-01-22 19:40:47 +0100

    Update Routing tests

--- admin/service/__tests__/App.test.tsx
+++ admin/service/__tests__/App.test.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ApolloLink } from 'apollo-link'
 
 class HttpLink {
   constructor(object: object) { }
 }
 
+class ApolloLinkMock {
+  public concat(object: object) { return new ApolloLink() }
+}
+
 jest.doMock('apollo-link-http', () => ({ HttpLink }))
+jest.doMock('apollo-link-context', () => ({ setContext: () => new ApolloLinkMock() }))
 
 describe('App Component', () => {
   it('render without crashing', () => {
--- admin/service/__tests__/Routing.test.tsx
+++ admin/service/__tests__/Routing.test.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { mount } from 'enzyme'
+import { Route } from 'react-router-dom'
 import { MemoryRouter } from 'react-router';
 
 import routeList from '@config/routeList'
 
-jest.doMock('../../page/LoginPage', () => LoginPageMock)
+jest.mock('../../page/LoginPage', () => LoginPageMock)
+jest.mock('../../page/DashboardPage', () => DashboardPageMock)
+jest.doMock('../../utility/PrivateRoute', () => Route)
+
 jest.mock('react-router-dom', () => {
   const originalModule = jest.requireActual('react-router-dom');
 
@@ -13,10 +17,10 @@ jest.mock('react-router-dom', () => {
     ...originalModule,
     BrowserRouter: ({ children }: { children: React.ReactChild }) => <div>{children}</div>
   };
-});
+})
 
 describe('Routing Service', () => {
-  it('should render LoginPage as default', () => {
+  it('should render DashboardPage as default', () => {
     const Routing = require('../Routing').default
 
     const wrapper = mount(
@@ -25,7 +29,7 @@ describe('Routing Service', () => {
       </MemoryRouter>
     )
 
-    expect(wrapper.find(LoginPageMock)).toHaveLength(1)
+    expect(wrapper.find(DashboardPageMock)).toHaveLength(1)
   })
 
   it('should render any route', () => {
@@ -51,6 +55,20 @@ describe('Routing Service', () => {
 
     expect(wrapper.find(LoginPageMock)).toHaveLength(1)
   })
+
+  it('should render DashboardPage', () => {
+    const Routing = require('../Routing').default
+
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[routeList.dashboardPageUrl]}>
+        <Routing />
+      </MemoryRouter>
+    )
+
+    expect(wrapper.find(DashboardPageMock)).toHaveLength(1)
+  })
 })
 
 const LoginPageMock = (): JSX.Element => <div>Login Page</div>
+
+const DashboardPageMock = (): JSX.Element => <div>Dashboard Page</div>
