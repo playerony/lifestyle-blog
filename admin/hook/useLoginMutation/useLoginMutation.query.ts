b9472dc3cf5a9bbd780eb1c21991a6568ee21cfc
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-01-25 20:42:08 +0100

    Remove usused code

--- admin/hook/useLoginMutation/useLoginMutation.query.ts
+++
@@ -1,9 +0,0 @@
-import gql from 'graphql-tag'
-
-export const LOGIN_MUTATION_QUERY = gql`
-  mutation LoginMutation($login: String!, $password: String!) {
-    login(login: $login, password: $password) {
-      token
-    }
-  }
-`
