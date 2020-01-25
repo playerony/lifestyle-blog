bb2adce32f3625c0c806373286be4945664adb16
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-01-25 21:00:18 +0100

    LoginPage - mobile - prevent sending request with same data

--- admin/component/LoginPage/mobile/index.tsx
+++ admin/component/LoginPage/mobile/index.tsx
@@ -1,3 +1,4 @@
+import { isEqual } from 'lodash'
 import React, { useState, ChangeEvent, FormEvent } from 'react'
 
 import Input from '@component/generic/Input'
@@ -9,17 +10,23 @@ import getFieldError from '@utility/getFieldError'
 
 import { StyledContentWrapper, StyledForm } from './LoginPage.style'
 
-const LoginPage = ({ onClick, errorData }: ILoginPage): JSX.Element => {
-  const [loginData, setLoginData] = useState<ILoginRequest>({ login: '', password: '' })
+const initialState: ILoginRequest = { login: '', password: '' }
 
-  const onInputChange = ({ target: { name, value } }: ChangeEvent<HTMLInputElement>): void =>
-    setLoginData(previousLoginData => ({ ...previousLoginData, [name]: value }))
+const LoginPage = ({ onClick, errorData }: ILoginPage): JSX.Element => {
+  const [loginData, setLoginData] = useState<ILoginRequest>(initialState)
+  const [requestLoginData, setRequestLoginData] = useState<ILoginRequest>(initialState)
+
+  const handleInputChange = ({ target: { name, value } }: ChangeEvent<HTMLInputElement>): void =>
+    setLoginData(prev => ({ ...prev, [name]: value }))
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
     event.preventDefault()
 
     onClick(loginData)
+    setRequestLoginData(loginData)
   }
+
+  const isButtonDisabled = (): boolean => isEqual(loginData, requestLoginData)
 
   return (
     <StyledContentWrapper>
@@ -30,7 +37,7 @@ const LoginPage = ({ onClick, errorData }: ILoginPage): JSX.Element => {
         <Input
           name="login"
           label="Login"
-          onChange={onInputChange}
+          onChange={handleInputChange}
           placeholder="What is your username?"
           errorMessage={getFieldError(errorData, 'login')}
         />
@@ -38,11 +45,15 @@ const LoginPage = ({ onClick, errorData }: ILoginPage): JSX.Element => {
           name="password"
           type="password"
           label="Password"
-          onChange={onInputChange}
+          onChange={handleInputChange}
           placeholder="What is your password?"
           errorMessage={getFieldError(errorData, 'password')}
         />
-        <Input type="submit" style={{ display: 'none' }} />
+        <Input
+          type="submit"
+          style={{ display: 'none' }}
+          disabled={isButtonDisabled()}
+        />
       </StyledForm>
     </StyledContentWrapper>
   )
