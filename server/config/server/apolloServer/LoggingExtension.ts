ee5f3029db3ef7af885d4e07466380e1a620d5cc
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-01 19:16:48 +0100

    Move LoggingExtension to separate file

---
+++ server/config/server/apolloServer/LoggingExtension.ts
@@ -0,0 +1,13 @@
+import { GraphQLExtension, GraphQLResponse } from 'graphql-extensions'
+
+import Logger from '@utility/Logger'
+
+class LoggingExtension extends GraphQLExtension {
+  willSendResponse({ graphqlResponse }: { graphqlResponse: GraphQLResponse }) {
+    if (Boolean(graphqlResponse?.errors)) {
+      Logger.database(graphqlResponse.errors![0].message)
+    }
+  }
+}
+  
+export default LoggingExtension
--- server/config/server/apolloServer/index.ts
+++ server/config/server/apolloServer/index.ts
@@ -1,26 +1,17 @@
 import { GraphQLSchema } from 'graphql'
 import { buildSchema } from 'type-graphql'
 import { ApolloServer } from 'apollo-server-express'
-import { GraphQLExtension, GraphQLResponse } from 'graphql-extensions'
 
 import UserResolver from '@resolver/UserResolver'
 import ImageResolver from '@resolver/ImageResolver'
 
-import Logger from '@utility/Logger'
+import LoggingExtension from './LoggingExtension'
 
 const buildGraphQLSchema = async (): Promise<GraphQLSchema> =>
   buildSchema({
     resolvers: [UserResolver, ImageResolver],
     nullableByDefault: true
   })
-
-class BasicLogging extends GraphQLExtension {
-  willSendResponse({ graphqlResponse }: { graphqlResponse: GraphQLResponse }) {
-    if (Boolean(graphqlResponse?.errors)) {
-      Logger.database(graphqlResponse.errors![0].message)
-    }
-  }
-}
 
 export default async (): Promise<ApolloServer> => {
   const schema = await buildGraphQLSchema()
@@ -30,6 +21,6 @@ export default async (): Promise<ApolloServer> => {
     context: ({ req }) => ({
       token: req?.headers?.authorization || ''
     }),
-    extensions: [() => new BasicLogging()]
+    extensions: [() => new LoggingExtension()]
   })
 }
