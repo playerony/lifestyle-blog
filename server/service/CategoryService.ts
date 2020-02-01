4fa9a76ef1da412895b35fc771dd7ce0613bc65c
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-01 21:10:09 +0100

    Improve category mapping

--- server/service/CategoryService.ts
+++ server/service/CategoryService.ts
@@ -2,10 +2,15 @@ import { Category } from '@model/Category'
 
 import { CategoryModel, CategoryRecordResult } from '@type/Category'
 
+const categoryMapping = (category: CategoryModel): CategoryRecordResult => ({
+  categoryId: category.categoryId!,
+  name: category.name!
+})
+
 export default class CategoryService {
   async findAll(): Promise<CategoryRecordResult[]> {
     const categoryList = await Category.findAll<CategoryModel>()
 
-    return categoryList.map(({ categoryId, name }) => ({ categoryId: categoryId!, name: name! }))
+    return categoryList.map(categoryMapping)
   }
 }
