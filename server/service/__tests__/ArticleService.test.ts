cf5983cf4efd4b464353b76816370e6e3f28bb79
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-05 23:13:27 +0100

    Simple tests for ArticleService

---
+++ server/service/__tests__/ArticleService.test.ts
@@ -0,0 +1,46 @@
+const { dataTypes: DataTypes } = require('sequelize-test-helpers')
+
+import { ArticleModel } from '@type/Article'
+
+const bulkCreateMock = jest.fn()
+
+jest.doMock('sequelize', () => {
+  class Sequelize {}
+
+  class Model {
+    public static init = jest.fn()
+    public static hasMany = jest.fn()
+    public static belongsTo = jest.fn()
+    public static afterCreate = jest.fn()
+    public static bulkCreate = bulkCreateMock
+    public static create = jest.fn().mockImplementation(() => ARTICLE_MOCK)
+  }
+
+  return {
+    Model,
+    DataTypes,
+    Sequelize
+  }
+})
+
+describe('ArticleService Service', () => {
+  describe('create Method', () => {
+    it('should return created articleId', async () => {
+      const ArticleService = require('../ArticleService').default
+      const articleService = new ArticleService()
+
+      const result = await articleService.create(ARTICLE_MOCK, 1)
+
+      expect(result).toEqual({ articleId: 1 })
+    })
+  })
+})
+
+const ARTICLE_MOCK: Partial<ArticleModel> = {
+  userId: 1,
+  imageId: 1,
+  articleId: 1,
+  title: '123',
+  content: '123',
+  description: '123'
+}
