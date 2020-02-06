998f9a4d0994619f5698e2afd0a10467be47ad69
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-06 09:36:45 +0100

    ImageResolver tests

---
+++ server/resolver/ImageResolver/__tests__/ImageResolver.test.ts
@@ -0,0 +1,95 @@
+import jwt from 'jsonwebtoken'
+import { FileUpload } from 'graphql-upload'
+import Substitute, { Arg } from '@fluffy-spoon/substitute'
+
+import ImageResolver from '../index'
+
+import ImageService from '@service/ImageService'
+
+import Context from '@type/Context'
+import { ImageAddResult } from '@type/Image'
+
+jest.mock('../../../config/keys', () => ({
+  jwtPrefix: 'Prefix',
+  appSecret: 'Secret test'
+}))
+
+import keys from '@config/keys'
+
+const _ImageService = Substitute.for<ImageService>()
+
+let resolver: ImageResolver
+
+describe('UserResolver Resolver', () => {
+  beforeEach(() => {
+    resolver = new ImageResolver(_ImageService)
+  })
+
+  beforeAll(() => {
+    _ImageService
+      .upload(Arg.any(), Arg.any())
+      .mimicks(async () => UPLOAD_METHOD_MOCK)
+  })
+
+  describe('uploadImage mutation', () => {
+    describe('should throw an error', () => {
+      it('if token is wrong', async () => {
+        const context: Context = {
+          token: ''
+        }
+
+        try {
+          await resolver.uploadImage(context, FILE_MOCK)
+        } catch (e) {
+          expect(e.message).toEqual('Authentication Error.')
+        }
+      })
+
+      it('if passed image data is wrong', async () => {
+        const token = jwt.sign({ userId: 1 }, keys.appSecret!)
+        const context: Context = {
+          token: `${keys.jwtPrefix} ${token}`
+        }
+
+        try {
+          await resolver.uploadImage(context, {
+            ...FILE_MOCK,
+            mimetype: 'wrong value'
+          })
+        } catch (e) {
+          expect(e.message).toEqual(
+            JSON.stringify({
+              filename: [],
+              mimetype: ['Provided values are not same.'],
+              encoding: [],
+              createReadStream: []
+            })
+          )
+        }
+      })
+    })
+
+    it('should return proper data', async () => {
+      const token = jwt.sign({ userId: 1 }, keys.appSecret!)
+      const context: Context = {
+        token: `${keys.jwtPrefix} ${token}`
+      }
+
+      const result = await resolver.uploadImage(context, FILE_MOCK)
+
+      expect(result).toEqual(UPLOAD_METHOD_MOCK)
+    })
+  })
+})
+
+const UPLOAD_METHOD_MOCK: ImageAddResult = {
+  imageId: 1,
+  filename: 'example.jpeg'
+}
+
+const FILE_MOCK: FileUpload = {
+  encoding: '7bit',
+  filename: 'test.jpeg',
+  mimetype: 'image/jpeg',
+  createReadStream: jest.fn()
+}
