db8dcfb1965a7a9db530a32b6a7b90cf34abea9a
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-07 21:06:12 +0100

    Upade contant config tests

--- admin/config/__tests__/constant.test.ts
+++ admin/config/__tests__/constant.test.ts
@@ -2,10 +2,18 @@ import * as constant from '../constant'
 
 describe('constant defuned variable list', () => {
   it('should return one value', () => {
-    expect(Object.keys(constant)).toHaveLength(1)
+    expect(Object.keys(constant)).toHaveLength(3)
   })
 
   it('should have defined AUTH_TOKEN property', () => {
     expect(constant.AUTH_TOKEN).toEqual('auth_token')
+  })
+
+  it('should have defined IMAGE_ACCEPT_TYPE property', () => {
+    expect(constant.IMAGE_ACCEPT_TYPE).toEqual('image/jpeg')
+  })
+
+  it('should have defined IMAGE_UPLOAD_ERROR_MESSAGE property', () => {
+    expect(constant.IMAGE_UPLOAD_ERROR_MESSAGE).toEqual('Something went wrong.')
   })
 })
