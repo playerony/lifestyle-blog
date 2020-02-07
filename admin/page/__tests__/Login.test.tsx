6990cb4b00e6b4f61af1c65974444778330c3730
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-07 14:54:43 +0100

    Improve imports

--- admin/page/__tests__/Login.test.tsx
+++ admin/page/__tests__/Login.test.tsx
@@ -17,18 +17,18 @@ jest.mock('react-router-dom', () => {
 import React from 'react'
 import { mount } from 'enzyme'
 
+import LoginPage from '../Login'
+
 import ILoginRequest from '@type/login/ILoginRequest'
 
 describe('Login Page', () => {
   it('should render', () => {
-    const LoginPage = require('../Login').default
     const wrapper = mount(<LoginPage />)
 
     expect(wrapper.exists()).toBeTruthy()
   })
 
   it('should render mobile view', () => {
-    const LoginPage = require('../Login').default
     const wrapper = mount(<LoginPage />)
 
     expect(wrapper.exists(LoginPageMobile)).toBeTruthy()
