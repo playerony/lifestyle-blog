958355472cab621edb6119d95691705e5cafe958
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-07 18:25:15 +0100

    Create useTimeout hook

---
+++ admin/hook/utility/useTimeout.ts
@@ -0,0 +1,22 @@
+import { MutableRefObject, useEffect, useRef } from 'react'
+
+function useTimeout(callback: Function, delay: number): void {
+  const savedCallback: MutableRefObject<Function | undefined> = useRef()
+
+  useEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  useEffect(() => {
+    const tick = () => {
+      if (savedCallback?.current) {
+        savedCallback.current()
+      }
+    }
+
+    const id = setTimeout(tick, delay)
+    return () => clearTimeout(id)
+  }, [delay])
+}
+
+export default useTimeout
