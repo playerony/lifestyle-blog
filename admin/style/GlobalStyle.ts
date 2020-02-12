669cdad78da97796fc33e01bd55f7a41301526f1
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-12 18:02:06 +0100

    GlobalStyle - fix

--- admin/style/GlobalStyle.ts
+++ admin/style/GlobalStyle.ts
@@ -2,11 +2,12 @@ import { createGlobalStyle } from 'styled-components'
 
 import { ThemeProps } from './theme'
 import getRem from './helper/getRem'
+import variable from '@style/variable'
 
 const GlobalStyle = createGlobalStyle`
   body {
-    font-family: ${({ theme }: ThemeProps) => theme.variable.font.normal};
-    background-color: ${({ theme }: ThemeProps) => theme.mode.color.background};
+    font-family: ${variable.font.normal};
+    background-color: ${({ theme }: ThemeProps) => theme.color.background};
   }
 
   label {
@@ -20,43 +21,43 @@ const GlobalStyle = createGlobalStyle`
   h1 {
     font-weight: bold;
     font-size: ${getRem(32)};
-    color: ${({ theme }: ThemeProps) => theme.variable.color.black};
-    font-family: ${({ theme }: ThemeProps) => theme.variable.font.bold};
+    color: ${variable.color.black};
+    font-family: ${variable.font.bold};
   }
 
   h2 {
     font-weight: bold;
     font-size: ${getRem(24)};
-    color: ${({ theme }: ThemeProps) => theme.variable.color.black};
-    font-family: ${({ theme }: ThemeProps) => theme.variable.font.bold};
+    color: ${variable.color.black};
+    font-family: ${variable.font.bold};
   }
 
   h3 {
     font-weight: bold;
     font-size: ${getRem(19)};
-    color: ${({ theme }: ThemeProps) => theme.variable.color.black};
-    font-family: ${({ theme }: ThemeProps) => theme.variable.font.bold};
+    color: ${variable.color.black};
+    font-family: ${variable.font.bold};
   }
 
   h4 {
     font-weight: bold;
     font-size: ${getRem(16)};
-    color: ${({ theme }: ThemeProps) => theme.variable.color.black};
-    font-family: ${({ theme }: ThemeProps) => theme.variable.font.bold};
+    color: ${variable.color.black};
+    font-family: ${variable.font.bold};
   }
 
   h5 {
     font-weight: bold;
     font-size: ${getRem(13)};
-    color: ${({ theme }: ThemeProps) => theme.variable.color.black};
-    font-family: ${({ theme }: ThemeProps) => theme.variable.font.bold};
+    color: ${variable.color.black};
+    font-family: ${variable.font.bold};
   }
 
   h6 {
     font-weight: bold;
     font-size: ${getRem(11)};
-    color: ${({ theme }: ThemeProps) => theme.variable.color.black};
-    font-family: ${({ theme }: ThemeProps) => theme.variable.font.bold};
+    color: ${variable.color.black};
+    font-family: ${variable.font.bold};
   }
 `
 
