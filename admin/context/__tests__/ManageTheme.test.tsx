6bbc1650a8d12202c78b61280816de6bfabf6651
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-12 17:18:17 +0100

    ManageTheme Context - tests

---
+++ admin/context/__tests__/ManageTheme.test.tsx
@@ -0,0 +1,23 @@
+import { mount } from 'enzyme'
+import React, { useContext } from 'react'
+
+import ManageThemeContext from '../ManageTheme'
+
+import EThemeMode from '@type/common/EThemeMode'
+
+describe('ManageThemeContext Context', () => {
+  it('should contain proper data', done => {
+    const Component = (): null => {
+      const context = useContext(ManageThemeContext)
+
+      expect(Object.keys(context)).toHaveLength(2)
+      expect(context.toggle).toBeDefined()
+      expect(context.mode).toEqual(EThemeMode.LIGHT)
+      done()
+
+      return null
+    }
+
+    mount(<Component />)
+  })
+})
