c1fcd7521a06f5c411b58b0b5b332eac22eff1af
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-12 17:37:58 +0100

    useTheme Hook - tests

---
+++ admin/hook/utility/__tests__/useTheme.test.tsx
@@ -0,0 +1,23 @@
+import { mount } from 'enzyme'
+import React from 'react'
+
+import useTheme from '@hook/utility/useTheme'
+
+import EThemeMode from '@type/common/EThemeMode'
+
+describe('useTheme Hook', () => {
+  it('should contain proper data', done => {
+    const Component = (): null => {
+      const context = useTheme()
+
+      expect(Object.keys(context)).toHaveLength(2)
+      expect(context.toggle).toBeDefined()
+      expect(context.mode).toEqual(EThemeMode.LIGHT)
+      done()
+
+      return null
+    }
+
+    mount(<Component />)
+  })
+})
