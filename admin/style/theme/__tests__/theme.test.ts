0b3144963d87755b223d544c8409d0a8d4bc5b8c
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-13 19:57:10 +0100

    getTheme Function - tests

---
+++ admin/style/theme/__tests__/theme.test.ts
@@ -0,0 +1,15 @@
+import getTheme from '..'
+import dark from '../dark'
+import light from '../light'
+
+import EThemeMode from '@type/common/EThemeMode'
+
+describe('getTheme Function', () => {
+  it('should return dark theme', () => {
+    expect(getTheme(EThemeMode.DARK)).toEqual(dark)
+  })
+
+  it('should return light theme', () => {
+    expect(getTheme(EThemeMode.LIGHT)).toEqual(light)
+  })
+})
