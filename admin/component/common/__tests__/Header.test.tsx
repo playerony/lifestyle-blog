773b5b2db47c7d2e331d8426837ef4111f262d7e
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-13 20:51:07 +0100

    Header - tests update

--- admin/component/common/__tests__/Header.test.tsx
+++ admin/component/common/__tests__/Header.test.tsx
@@ -6,6 +6,16 @@ import Switch from '../../generic/Switch'
 
 import ThemeProviderMock from '@utility/ThemeProviderMock'
 
+jest.mock('react-router-dom', () => {
+  const originalModule = jest.requireActual('react-router-dom')
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    useHistory: () => ({ push: jest.fn() })
+  };
+})
+
 const mountComponent = (element: ReactElement): ReactWrapper =>
   mount(<ThemeProviderMock>{element}</ThemeProviderMock>)
 
@@ -31,9 +41,15 @@ describe('Header Component', () => {
   it('should render moon logo', () => {
     const wrapper = mountComponent(<Header />)
 
-    expect(wrapper.find('use').last().props().xlinkHref).toEqual('#moon')
+    expect(wrapper.find('use').get(2).props.xlinkHref).toEqual('#moon')
   })
 
+  it('should render logout logo', () => {
+    const wrapper = mountComponent(<Header />)
+
+    expect(wrapper.find('svg').last().props().onClick).toBeDefined()
+    expect(wrapper.find('use').last().props().xlinkHref).toEqual('#logout')
+  })
 
   it('should render Switch component', () => {
     const wrapper = mountComponent(<Header />)
