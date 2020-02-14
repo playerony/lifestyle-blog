d344ad64351e14ee8ad1e8a3768ed2ae72a6d0a2
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-14 22:27:03 +0100

    LoginPageDesktop - tests

--- admin/component/login/desktop/__tests__/LoginPageDesktop.test.tsx
+++ admin/component/login/desktop/__tests__/LoginPageDesktop.test.tsx
@@ -1,26 +1,39 @@
 import React from 'react'
 import { mount, ReactWrapper } from 'enzyme'
+import { act } from '@testing-library/react-hooks'
 
-import LoginPageMobile from '..'
+import LoginPageDesktop from '..'
 import LoginForm from '../LoginForm'
+import LogoWrapper from '../LogoWrapper'
 
 import ThemeProviderMock from '@utility/ThemeProviderMock'
 
 import ILoginRequest from '@type/login/ILoginRequest'
 import TResponseError from '@type/common/TResponseError'
 
+jest.useFakeTimers()
+
 const mountComponent = (element: React.ReactElement): ReactWrapper =>
   mount(<ThemeProviderMock>{element}</ThemeProviderMock>)
 
-describe('LoginPageMobile Component', () => {
+describe('LoginPageDesktop Component', () => {
   it('should render', () => {
-    const wrapper = mountComponent(<LoginPageMobile {...LOGIN_PAGE_PROPS_MOCK} />)
+    const wrapper = mountComponent(<LoginPageDesktop {...LOGIN_PAGE_PROPS_MOCK} />)
 
     expect(wrapper.exists()).toBeTruthy()
   })
 
+  it('should render LogoWrapper', () => {
+    const wrapper = mountComponent(<LoginPageDesktop {...LOGIN_PAGE_PROPS_MOCK} />)
+
+    expect(wrapper.exists(LogoWrapper)).toBeTruthy()
+  })
+
   it('should render LoginForm', () => {
-    const wrapper = mountComponent(<LoginPageMobile {...LOGIN_PAGE_PROPS_MOCK} />)
+    const wrapper = mountComponent(<LoginPageDesktop {...LOGIN_PAGE_PROPS_MOCK} />)
+
+    act(() => { jest.advanceTimersByTime(3000) })
+    wrapper.mount()
 
     expect(wrapper.find(LoginForm).props())
       .toEqual({ onClick: jasmine.any(Function), errorData: ERROR_DATA_MOCK })
