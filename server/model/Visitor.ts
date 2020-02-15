b68b529929039265a7470d77c4e38c4bafb27244
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-15 19:48:40 +0100

    Create Visitor model

---
+++ server/model/Visitor.ts
@@ -0,0 +1,36 @@
+import { Model, DataTypes } from 'sequelize'
+
+import { Article } from './Article'
+
+import { sequelize } from '@config/database/sequelize'
+
+export class Visitor extends Model {}
+
+Visitor.init(
+  {
+    visitorId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    articleId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        key: 'articleId',
+        model: Article
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    },
+    ipAddress: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      validate: {
+        isIP: true,
+        notEmpty: true
+      }
+    }
+  },
+  { sequelize }
+)
