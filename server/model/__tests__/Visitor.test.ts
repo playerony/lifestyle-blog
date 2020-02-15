8d7e456ccb97d63cde4510cff848b9ba2b1654cf
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-15 22:08:55 +0100

    Visitor Model - tests

---
+++ server/model/__tests__/Visitor.test.ts
@@ -0,0 +1,51 @@
+const { dataTypes: DataTypes } = require('sequelize-test-helpers')
+
+class Sequelize {}
+
+const initMock = jest.fn()
+
+jest.doMock('sequelize', () => {
+  class Model {
+    public static init = initMock
+  }
+
+  return {
+    Model,
+    DataTypes,
+    Sequelize
+  }
+})
+
+describe('Visitor Model', () => {
+  it('should call Visitor.init with the correct parameters', () => {
+    require('../Visitor')
+
+    expect(initMock).toBeCalledWith(
+      {
+        visitorId: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        articleId: {
+          type: DataTypes.INTEGER
+        },
+        ipAddress: {
+          type: DataTypes.STRING(50),
+          allowNull: false,
+          validate: {
+            isIP: true,
+            notEmpty: true
+          }
+        },
+        userAgent: {
+          type: DataTypes.STRING,
+          allowNull: false
+        }
+      },
+      {
+        sequelize: new Sequelize()
+      }
+    )
+  })
+})
