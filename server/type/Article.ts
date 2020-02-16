1e3fc9457ec8977770a651ef665d84c00760c99a
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-16 20:21:23 +0100

    Add Separate typing for Article

--- server/type/Article.ts
+++ server/type/Article.ts
@@ -1,8 +1,8 @@
 import { Model } from 'sequelize'
 import { Int, Field, ObjectType, InputType } from 'type-graphql'
 
-import { ImageModel } from './Image'
-import { CategoryModel } from './Category'
+import { ImageModel, ImageType } from './Image'
+import { CategoryModel, CategoryType } from './Category'
 
 @ObjectType()
 export class ArticleModel extends Model {
@@ -29,6 +29,39 @@ export class ArticleModel extends Model {
 
   @Field(type => [CategoryModel])
   categoryList?: CategoryModel[]
+
+  @Field()
+  createdAt?: Date
+
+  @Field()
+  updatedAt?: Date
+}
+
+@ObjectType()
+export class ArticleType {
+  @Field()
+  articleId?: number
+
+  @Field()
+  userId?: number
+
+  @Field()
+  imageId?: number
+
+  @Field()
+  title?: string
+
+  @Field()
+  description?: string
+
+  @Field()
+  content?: string
+
+  @Field(type => ImageType)
+  image?: ImageType | null
+
+  @Field(type => [CategoryType])
+  categoryList?: CategoryType[] | null
 
   @Field()
   createdAt?: Date
