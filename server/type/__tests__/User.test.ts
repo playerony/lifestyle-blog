a630c7f58463dced96622eb5f7d47cc9a8398203
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-20 18:08:24 +0100

    User types - tests

---
+++ server/type/__tests__/User.test.ts
@@ -0,0 +1,59 @@
+import * as userTypeList from '../User'
+import { UserType, UserModel, UserAddResult } from '../User'
+
+describe('User Types', () => {
+  it('should contain three objects', () => {
+    expect(Object.keys(userTypeList)).toHaveLength(3)
+  })
+
+  describe('UserType Type', () => {
+    it('should contain proper fields', () => {
+      const data: Required<UserType> = {
+        userId: 1,
+        login: 'login',
+        password: 'password',
+        createdAt: new Date('2020'),
+        updatedAt: new Date('2020')
+      }
+
+      expect(Object.keys(data)).toEqual([
+        'userId',
+        'login',
+        'password',
+        'createdAt',
+        'updatedAt'
+      ])
+    })
+  })
+
+  describe('UserAddResult Type', () => {
+    it('should contain proper fields', () => {
+      const data: Required<UserAddResult> = {
+        user: {},
+        token: 'token'
+      }
+
+      expect(Object.keys(data)).toEqual(['user', 'token'])
+    })
+  })
+
+  describe('UserModel Type', () => {
+    it('should contain proper fields', () => {
+      const data: Partial<UserModel> = {
+        userId: 1,
+        login: 'login',
+        password: 'password',
+        createdAt: new Date('2020'),
+        updatedAt: new Date('2020')
+      }
+
+      expect(Object.keys(data)).toEqual([
+        'userId',
+        'login',
+        'password',
+        'createdAt',
+        'updatedAt'
+      ])
+    })
+  })
+})
