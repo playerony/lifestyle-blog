67ab096681b8dbf8ab2e8009b6648bb0b52081fd
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-22 19:27:18 +0100

    isMobileUserAgent - tests

---
+++ admin/utility/__tests__/isMobileUserAgent.test.ts
@@ -0,0 +1,15 @@
+import isMobileUserAgent from '../isMobileUserAgent'
+
+describe('isMobileUserAgent', () => {
+  it('shoudl return true if userAgent is from the mobile device', () => {
+    USER_AGENT_LIST_MOCK.map(userAgent => {
+      expect(isMobileUserAgent(userAgent)).toBeTruthy()
+    })
+  })
+})
+
+const USER_AGENT_LIST_MOCK = [
+  'Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36',
+  'Mozilla/5.0 (Linux; Android 7.0; SM-A310F Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.91 Mobile Safari/537.36 OPR/42.7.2246.114996',
+  'Mozilla/5.0 (Linux; Android 5.1.1; SM-N750K Build/LMY47X; ko-kr) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Puffin/6.0.8.15804AP'
+]
