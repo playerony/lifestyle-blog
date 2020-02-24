3d6f799c178e76cc1b0ecf1fae0fd8c9d7aaf083
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-02-24 19:41:15 +0100

    updateArticleValidation - tests

--- server/resolver/ArticleResolver/__tests__/ArticleResolver.validator.test.ts
+++ server/resolver/ArticleResolver/__tests__/ArticleResolver.validator.test.ts
@@ -1,6 +1,7 @@
 import {
   articleByIdValidation,
-  createArticleValidation
+  createArticleValidation,
+  updateArticleValidation
 } from '../ArticleResolver.validator'
 
 describe('ArticleResolver.validator Functions', () => {
@@ -89,6 +90,123 @@ describe('ArticleResolver.validator Functions', () => {
 
         try {
           createArticleValidation(data as any)
+        } catch (e) {
+          const { categoryIdList } = JSON.parse(e.message)
+
+          expect(categoryIdList).toEqual([
+            'Provided value does not exist.',
+            'Provided value is not an array.',
+            `Provided value's length is shorter than 1.`
+          ])
+        }
+      })
+    })
+  })
+
+  describe('updateArticleValidation Function', () => {
+    describe('articleId Field', () => {
+      it('should contain proper validators', () => {
+        const data = {
+          imageId: null
+        }
+
+        try {
+          updateArticleValidation(null as any, data as any)
+        } catch (e) {
+          const { articleId } = JSON.parse(e.message)
+
+          expect(articleId).toEqual([
+            'Provided value does not exist.',
+            'Provided value is not a number.'
+          ])
+        }
+      })
+    })
+
+    describe('imageId Field', () => {
+      it('should contain proper validators', () => {
+        const data = {
+          imageId: null
+        }
+
+        try {
+          updateArticleValidation(1, data as any)
+        } catch (e) {
+          const { imageId } = JSON.parse(e.message)
+
+          expect(imageId).toEqual([
+            'Provided value does not exist.',
+            'Provided value is not a number.'
+          ])
+        }
+      })
+    })
+
+    describe('title Field', () => {
+      it('should contain proper validators', () => {
+        const data = {
+          title: null
+        }
+
+        try {
+          updateArticleValidation(1, data as any)
+        } catch (e) {
+          const { title } = JSON.parse(e.message)
+
+          expect(title).toEqual([
+            'Provided value does not exist.',
+            `Provided value's length is longer than 30.`
+          ])
+        }
+      })
+    })
+
+    describe('description Field', () => {
+      it('should contain proper validators', () => {
+        const data = {
+          description: null
+        }
+
+        try {
+          updateArticleValidation(1, data as any)
+        } catch (e) {
+          const { description } = JSON.parse(e.message)
+
+          expect(description).toEqual([
+            'Provided value does not exist.',
+            `Provided value's length is longer than 100.`
+          ])
+        }
+      })
+    })
+
+    describe('content Field', () => {
+      it('should contain proper validators', () => {
+        const data = {
+          content: null
+        }
+
+        try {
+          updateArticleValidation(1, data as any)
+        } catch (e) {
+          const { content } = JSON.parse(e.message)
+
+          expect(content).toEqual([
+            'Provided value does not exist.',
+            `Provided value's length is shorter than 160.`
+          ])
+        }
+      })
+    })
+
+    describe('categoryIdList Field', () => {
+      it('should contain proper validators', () => {
+        const data = {
+          categoryIdList: null
+        }
+
+        try {
+          updateArticleValidation(1, data as any)
         } catch (e) {
           const { categoryIdList } = JSON.parse(e.message)
 
