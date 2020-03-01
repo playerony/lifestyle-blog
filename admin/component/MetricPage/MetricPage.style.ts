05cd8f34b23c60f7c6c6fe2b76ebf9c2ca467324
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-01 19:25:09 +0100

    MetricPage - add back button

---
+++ admin/component/MetricPage/MetricPage.style.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+
+import variable from '@style/variable'
+import { ThemeProps } from '@style/theme'
+import { boxShadow, borderRadius, respondTo } from '@style/mixin'
+
+export const StyledHeader = styled.h1`
+  text-align: center;
+`
+
+export const StyledChartWrapper = styled.section`
+  padding: ${variable.paddingSmall}px;
+  margin-top: ${variable.paddingBig}px;
+  background-color: ${({ theme }: ThemeProps) => theme.color.card};
+
+  ${borderRadius('4px')}
+  ${boxShadow('0 2px 8px 0 rgba(0, 0, 0, 0.16)')}
+
+  ${respondTo.mediumScreen`
+    margin-top: ${variable.paddingMedium}px;
+  `}
+
+  ${respondTo.smallScreen`
+    margin-top: ${variable.paddingSmall}px;
+  `}
+`
+
+export const StyledArrowIcon = styled.svg`
+  width: 32px;
+  height: 40px;
+  fill: ${variable.color.white};
+
+  ${respondTo.mediumScreen`
+    width: 27px;
+    height: 30px;
+  `}
+
+  ${respondTo.smallScreen`
+    width: 20px;
+    height: 24px;
+  `}
+`
--- admin/component/MetricPage/index.tsx
+++ admin/component/MetricPage/index.tsx
@@ -1,20 +1,41 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import { Bar, Doughnut } from 'react-chartjs-2'
 
+import Button from '../generic/Button'
 import Container from '../common/Container'
 
 import { IMetricPageProps } from './MetricPage.type'
 
-import metricTransformator from '@admin/transformator/metricTransformator'
+import metricTransformator from '@transformator/metricTransformator'
+
+import { StyledArrowIcon } from './MetricPage.style'
 
 const MetricPage = ({ visitorList = [] }: IMetricPageProps): JSX.Element => {
-  const { lineChartData, blogVisitorChartData, visitorDeviceChartData } = metricTransformator(visitorList)
+  const history = useHistory()
+  const {
+    lineChartData,
+    blogVisitorChartData,
+    visitorDeviceChartData
+  } = metricTransformator(visitorList)
+
+  const handleButtonClick = (): void =>
+    history.goBack()
 
   return (
     <Container>
       <Bar data={lineChartData} />
       <Doughnut data={blogVisitorChartData} />
       <Doughnut data={visitorDeviceChartData} />
+      <Button
+        circle={true}
+        floating="left"
+        onClick={handleButtonClick}
+      >
+        <StyledArrowIcon>
+          <use xlinkHref="#left-arrow-button" />
+        </StyledArrowIcon>
+      </Button>
     </Container>
   )
 }
