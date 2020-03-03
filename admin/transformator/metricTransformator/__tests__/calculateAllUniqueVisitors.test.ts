bbaa7ccb01d12f6bb8db48ec90cbbde7c254baf7
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-03 18:05:39 +0100

    calculateAllUniqueVisitors - tests

---
+++ admin/transformator/metricTransformator/__tests__/calculateAllUniqueVisitors.test.ts
@@ -0,0 +1,29 @@
+import IVisitor from '@type/visitor/IVisitor'
+
+import calculateAllUniqueVisitors from '../calculateAllUniqueVisitors'
+
+describe('calculateAllUniqueVisitors Function', () => {
+  it('should return zero for empty array', () => {
+    const result = calculateAllUniqueVisitors([])
+
+    expect(result).toEqual(0)
+  })
+
+  it('should return number of unqiue visitors', () => {
+    const result = calculateAllUniqueVisitors(VISITOR_LIST_MOCK)
+
+    expect(result).toEqual(2)
+  })
+})
+
+const VISITOR_LIST_MOCK: IVisitor[] = [
+  {
+    ipAddress: 'ipAddress1'
+  },
+  {
+    ipAddress: 'ipAddress2'
+  },
+  {
+    ipAddress: 'ipAddress1'
+  }
+]
