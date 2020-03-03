de062888ba020f7bf473a01837a9ab4344ed545d
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-03 19:32:15 +0100

    calculateArticleListVisitors - tests

---
+++ admin/transformator/metricTransformator/__tests__/calculateArticleListVisitors.test.ts
@@ -0,0 +1,59 @@
+import IVisitor from '@type/visitor/IVisitor'
+
+import calculateArticleListVisitors from '../calculateArticleListVisitors'
+
+describe('calculateArticleListVisitors Function', () => {
+  describe('should return zero', () => {
+    it('for empty array', () => {
+      const result = calculateArticleListVisitors([])
+
+      expect(result).toEqual(0)
+    })
+
+    it('for article page visitors', () => {
+      const result = calculateArticleListVisitors(VISITOR_LIST_MOCK)
+
+      expect(result).toEqual(0)
+    })
+  })
+
+  it('should return number of article page visitors', () => {
+    const result = calculateArticleListVisitors(ARTICLE_PAGE_VISITOR_LIST_MOCK)
+
+    expect(result).toEqual(2)
+  })
+})
+
+const ARTICLE_PAGE_VISITOR_LIST_MOCK: IVisitor[] = [
+  {
+    articleId: null,
+    ipAddress: 'ipAddress1'
+  },
+  {
+    articleId: null,
+    ipAddress: 'ipAddress2'
+  }
+]
+
+const VISITOR_LIST_MOCK: IVisitor[] = [
+  {
+    articleId: 1,
+    ipAddress: 'ipAddress1'
+  },
+  {
+    articleId: 2,
+    ipAddress: 'ipAddress2'
+  },
+  {
+    articleId: 3,
+    ipAddress: 'ipAddress1'
+  },
+  {
+    articleId: 3,
+    ipAddress: 'ipAddress2'
+  },
+  {
+    articleId: 3,
+    ipAddress: 'ipAddress2'
+  }
+]
