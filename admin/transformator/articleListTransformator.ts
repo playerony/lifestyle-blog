ea0fd06e690d2fbf195f70162036927212dc8907
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-04 21:57:22 +0100

    articleListTransformator fix

--- admin/transformator/articleListTransformator.ts
+++ admin/transformator/articleListTransformator.ts
@@ -32,9 +32,7 @@ export default (data?: {
     const todayVisitor = filteredVisitorList.filter(element => {
       const differenceInTime =
         new Date().getTime() - new Date(element.createdAt!).getTime()
-      const differenceInDays = differenceInTime / (100000 * 3600 * 24)
-
-      console.warn(differenceInDays)
+      const differenceInDays = differenceInTime / (1000 * 3600 * 24)
 
       return differenceInDays <= 1
     }).length
