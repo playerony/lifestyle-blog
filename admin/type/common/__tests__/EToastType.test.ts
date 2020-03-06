60aa5c6d1188f26766b3cee66991cb2cd8d7ed7f
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-06 21:49:22 +0100

    EToastType - update tests

--- admin/type/common/__tests__/EToastType.test.ts
+++ admin/type/common/__tests__/EToastType.test.ts
@@ -2,7 +2,7 @@ import EToastType from '../EToastType'
 
 describe('EToastType Enum', () => {
   it('should contain two options', () => {
-    expect(Object.keys(EToastType)).toHaveLength(2)
+    expect(Object.keys(EToastType)).toHaveLength(3)
   })
 
   it('should contain SUCCESS property', () => {
@@ -11,5 +11,9 @@ describe('EToastType Enum', () => {
 
   it('should contain ERROR property', () => {
     expect(EToastType.ERROR).toEqual('ERROR')
+  })
+
+  it('should contain INFO property', () => {
+    expect(EToastType.INFO).toEqual('INFO')
   })
 })
