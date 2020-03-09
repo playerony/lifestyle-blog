0b791b5616df5836b76c7a8e8a1cff7976ae602a
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-09 20:53:25 +0100

    ArticleCreatePage Component - cleanup

--- admin/component/article/ArticleCreatePage/ArticleCreatePage.style.ts
+++ admin/component/article/ArticleCreatePage/ArticleCreatePage.style.ts
@@ -1,8 +1,17 @@
 import styled from 'styled-components'
 
 import variable from '@style/variable'
+import { respondTo } from '@style/mixin'
 
 export const StyledHeader = styled.h1`
   text-align: center;
-  padding-bottom: ${variable.paddingMedium}px;
+  padding-bottom: ${variable.paddingBig}px;
+
+  ${respondTo.mediumScreen`
+    padding-bottom: ${variable.paddingMedium}px;
+  `}
+
+  ${respondTo.smallScreen`
+    padding-bottom: ${variable.paddingSmall}px;
+  `}
 `
