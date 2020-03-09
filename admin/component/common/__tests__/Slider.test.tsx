b5cc14f54458741efd2500112f9b64020ef9355f
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-09 19:36:04 +0100

    Slick Component - tests

---
+++ admin/component/common/__tests__/Slider.test.tsx
@@ -0,0 +1,34 @@
+import Slick from 'react-slick'
+import React, { ReactElement } from 'react'
+import { mount, ReactWrapper } from 'enzyme'
+
+import Slider from '../Slider'
+
+import ThemeProviderMock from '@component/utility/ThemeProviderMock'
+
+const mountComponent = (element: ReactElement): ReactWrapper =>
+  mount(<ThemeProviderMock>{element}</ThemeProviderMock>)
+
+describe('Slider Component', () => {
+  it('should render', () => {
+    const wrapper = mountComponent(<Slider><SliderElementMock /></Slider>)
+
+    expect(wrapper.exists()).toBeTruthy()
+  })
+
+  describe('Slick Component', () => {
+    it('should render', () => {
+      const wrapper = mountComponent(<Slider><SliderElementMock /></Slider>)
+
+      expect(wrapper.exists(Slick)).toBeTruthy()
+    })
+
+    it('should be wrapped with div element', () => {
+      const wrapper = mountComponent(<Slider><SliderElementMock /></Slider>)
+
+      expect(wrapper.find(Slick).parent().type()).toEqual('div')
+    })
+  })
+})
+
+const SliderElementMock = (): JSX.Element => <h1>Slider Element Mock</h1>
