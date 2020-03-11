1aadfd5a40f23814bc82c63bfba3436e14c6dadc
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-11 18:52:44 +0100

    Improve ArticleList header RWD

--- admin/component/article/ArticleListPage/Dashboard/ArticleList/ArticleList.style.ts
+++ admin/component/article/ArticleListPage/Dashboard/ArticleList/ArticleList.style.ts
@@ -1,5 +1,20 @@
 import styled from 'styled-components'
 
+import variable from '@style/variable'
+import { respondTo } from '@style/mixin'
+
 export const StyledLabel = styled.h2`
-  margin: 20px 0;
+  margin: ${variable.paddingBig}px 0;
+
+  ${respondTo.mediumScreen`
+    margin: ${variable.paddingMedium}px 0;
+  `}
+
+  ${respondTo.smallScreen`
+    margin: ${variable.paddingSmall}px 0;
+  `}
+
+  ${respondTo.mobileScreen`
+    margin: ${variable.paddingSmall / 2}px 0;
+  `}
 `
