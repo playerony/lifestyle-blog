f1d82c3bdf32ee97641869e1cf51547d187e74f8
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-13 17:18:08 +0100

    useToggleArticlePublicFlag hook - tests

---
+++ admin/hook/article/__tests__/useToggleArticlePublicFlag.test.tsx
@@ -0,0 +1,72 @@
+import { mount } from 'enzyme'
+import React, { useState, useEffect } from 'react'
+
+import ApolloProviderMock from '@component/utility/ApolloProviderMock'
+
+import useToggleArticlePublicFlag from '../useToggleArticlePublicFlag'
+
+import { TOGGLE_ARTICLE_PUBLIC_FLAG_MUTATION } from '../useToggleArticlePublicFlag/useToggleArticlePublicFlag.query'
+
+describe('useToggleArticlePublicFlag Hook', () => {
+  beforeAll(() => {
+    console.error = jest.fn()
+  })
+
+  it('import useToggleArticlePublicFlag', () => {
+    expect(typeof useToggleArticlePublicFlag).toBe('function')
+  })
+
+  it('should keep data as undefined until data is actually returned', done => {
+    const Component = (): null => {
+      const [data, setData] = useState<any>()
+
+      const toggleArticlePublicFlag = useToggleArticlePublicFlag()
+
+      useEffect(() => {
+        const doUpdate = async () => {
+          const result = await toggleArticlePublicFlag(1, true)
+
+          setData(result)
+        }
+
+        doUpdate()
+      }, [])
+
+      if (!data) {
+        expect(data).toEqual(undefined)
+      } else {
+        expect(data.data).toEqual(EDIT_ARTICLE_RESULT_DATA)
+        done()
+      }
+
+      return null
+    }
+
+    mount(
+      <ApolloProviderMock mockList={EDIT_ARTICLE_MOCK}>
+        <Component />
+      </ApolloProviderMock>
+    )
+  })
+})
+
+const EDIT_ARTICLE_RESULT_DATA = {
+  toggleArticlePublicFlag: {
+    articleId: 1,
+    isPublic: true,
+    __typename: 'Article'
+  }
+}
+
+const EDIT_ARTICLE_MOCK = [
+  {
+    request: {
+      query: TOGGLE_ARTICLE_PUBLIC_FLAG_MUTATION,
+      variables: {
+        articleId: 1,
+        isPublic: true
+      }
+    },
+    result: { data: EDIT_ARTICLE_RESULT_DATA }
+  }
+]
