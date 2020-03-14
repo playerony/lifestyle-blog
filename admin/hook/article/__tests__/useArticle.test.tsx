3e75028e2fd86e1b82290906a4d70772f51f1635
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-14 20:24:50 +0100

    useArticle hook - update tests

--- admin/hook/article/__tests__/useArticle.test.tsx
+++ admin/hook/article/__tests__/useArticle.test.tsx
@@ -1,27 +1,49 @@
 import React from 'react'
+import { MockedProvider } from '@apollo/react-testing'
 import { renderHook } from '@testing-library/react-hooks'
-
-import ApolloProviderMock from '@component/utility/ApolloProviderMock'
 
 import useArticle from '../useArticle'
 
 import { ARTICLE_QUERY } from '../useArticle/useArticle.query'
 
 describe('useArticle Hook', () => {
+  beforeAll(() => {
+    console.error = jest.fn()
+  })
+
   it('import useArticle', () => {
     expect(typeof useArticle).toBe('function')
   })
 
-  it('should keep data as null until data is actually returned', async () => {
+  it('should return proper data if loading status is false', async () => {
     const wrapper = ({ children }: any) =>
-      <ApolloProviderMock mockList={ARTICLE_MOCK}>{children}</ApolloProviderMock>
+      <MockedProvider mocks={ARTICLE_MOCK}>{children}</MockedProvider>
 
     const { result, waitForNextUpdate } = renderHook(() => useArticle(1), { wrapper })
 
-    expect(result.current).toBeNull()
-
     await waitForNextUpdate()
-    expect(result.current).toEqual(ARTICLE_RESULT_DATA.articleById)
+    expect(result.current).toEqual({ data: ARTICLE_RESULT_DATA.articleById, loading: false })
+  })
+
+  describe('return undefined as data', () => {
+    it('if loading status is true', () => {
+      const wrapper = ({ children }: any) =>
+        <MockedProvider mocks={ARTICLE_MOCK}>{children}</MockedProvider>
+
+      const { result } = renderHook(() => useArticle(1), { wrapper })
+
+      expect(result.current).toEqual({ data: undefined, loading: true })
+    })
+
+    it('should return if data does not exists', async () => {
+      const wrapper = ({ children }: any) =>
+        <MockedProvider mocks={ARTICLE_MOCK}>{children}</MockedProvider>
+
+      const { result, waitForNextUpdate } = renderHook(() => useArticle(2), { wrapper })
+
+      await waitForNextUpdate()
+      expect(result.current).toEqual({ data: undefined, loading: false })
+    })
   })
 })
 
