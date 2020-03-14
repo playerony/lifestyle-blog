d6d7b73269eae56c4000f18df9e3e9b890adf75d
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-14 19:50:52 +0100

    useLogList hook - cleanup

--- admin/hook/log/useLogList/index.ts
+++ admin/hook/log/useLogList/index.ts
@@ -1,11 +1,11 @@
 import { useQuery } from '@apollo/react-hooks'
 
-import ILog from '@type/log/ILog'
+import { IResult } from './useLogList.type'
 
 import { LOG_LIST_QUERY } from './useLogList.query'
 
 const useLogList = () => {
-  const { data, loading } = useQuery<{ logList: ILog[] }>(LOG_LIST_QUERY, {
+  const { data, loading } = useQuery<IResult>(LOG_LIST_QUERY, {
     fetchPolicy: 'cache-first'
   })
 
---
+++ admin/hook/log/useLogList/useLogList.type.ts
@@ -0,0 +1,5 @@
+import ILog from '@type/log/ILog'
+
+export interface IResult {
+  logList: ILog[]
+}
