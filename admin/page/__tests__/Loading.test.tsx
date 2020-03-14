a771534a9d4e02a03857137bd4aa1259c36aa4d7
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-14 20:42:53 +0100

    Loading Page - fix tests

--- admin/page/__tests__/Loading.test.tsx
+++ admin/page/__tests__/Loading.test.tsx
@@ -3,10 +3,11 @@ import { mount } from 'enzyme'
 
 import LoadingPage from '../Loading'
 import Loader from '@component/common/Loader'
+import ThemeProviderMock from '@component/utility/ThemeProviderMock'
 
 describe('Loading Page', () => {
   it('should render', () => {
-    const wrapper = mount(<LoadingPage />)
+    const wrapper = mount(<ThemeProviderMock><LoadingPage /></ThemeProviderMock>)
 
     expect(wrapper).toBeDefined()
     expect(wrapper.find(Loader).length).toEqual(1)
