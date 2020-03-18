c16eece63b4dbed6b362c7b43b5d1702ef3494fb
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-18 18:55:25 +0100

    Theme Context - update tests

--- admin/context/__tests__/Theme.test.tsx
+++ admin/context/__tests__/Theme.test.tsx
@@ -1,14 +1,14 @@
 import { mount } from 'enzyme'
 import React, { useContext } from 'react'
 
-import { ManageThemeContext } from '../ManageTheme'
+import { ThemeContext } from '../Theme'
 
 import EThemeMode from '@type/common/EThemeMode'
 
-describe('ManageTheme Context', () => {
-  it('should contain proper data as default', done => {
+describe('Theme Context', () => {
+  it('should contain proper default data', done => {
     const Component = (): null => {
-      const context = useContext(ManageThemeContext)
+      const context = useContext(ThemeContext)
 
       expect(Object.keys(context)).toHaveLength(2)
       expect(context.toggle).toBeDefined()
