e010a487b10bd82896c08132c85cba2a51d69846
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-19 20:06:33 +0100

    Improve Error Page layout

--- admin/page/Error/Error.style.ts
+++ admin/page/Error/Error.style.ts
@@ -1,6 +1,15 @@
 import styled from 'styled-components'
 
-import { flexbox, justifyContent, alignItems } from '@style/mixin'
+import {
+  flexbox,
+  respondTo,
+  transition,
+  alignItems,
+  flexDirection,
+  justifyContent
+} from '@style/mixin'
+import variable from '@style/variable'
+import getRem from '@style/helper/getRem'
 
 export const StyledWrapper = styled.div`
   width: 100%;
@@ -8,5 +17,37 @@ export const StyledWrapper = styled.div`
 
   ${flexbox()}
   ${alignItems('center')}
+  ${flexDirection('column')}
   ${justifyContent('center')}
+`
+
+export const StyledCode = styled.h1`
+  font-size: ${getRem(100)};
+  padding-top: ${variable.paddingMedium}px;
+
+  ${transition('color 500ms linear')}
+
+  ${respondTo.largeScreen`
+    font-size: ${getRem(90)};
+  `}
+
+  ${respondTo.mediumScreen`
+    font-size: ${getRem(75)};
+  `}
+
+  ${respondTo.smallScreen`
+    font-size: ${getRem(50)};
+  `}
+
+  ${respondTo.mobileScreen`
+    font-size: ${getRem(45)};
+  `}
+`
+
+export const StyledDescription = styled.label`
+  padding-bottom: ${variable.paddingMedium}px;
+
+  ${respondTo.smallScreen`
+    padding-bottom: ${variable.paddingSmall}px;
+  `}
 `
--- admin/page/Error/index.tsx
+++ admin/page/Error/index.tsx
@@ -2,25 +2,28 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 
 import Button from '@component/generic/Button'
+import Container from '@component/common/Container'
 
 import getErrorMessage from './getErrorMessage'
 
-import { StyledWrapper } from './Error.style'
+import { StyledCode, StyledWrapper, StyledDescription } from './Error.style'
 
 const Error = (): JSX.Element => {
   const { code } = useParams<{ code: string }>()
 
   return (
-    <StyledWrapper>
-      <svg>
-        <use xlinkHref="#robot" />
-      </svg>
-      <h1>{code}</h1>
-      <label>{getErrorMessage(code)}</label>
-      <Button>
-        Refresh
-      </Button>
-    </StyledWrapper>
+    <Container>
+      <StyledWrapper>
+        <svg>
+          <use xlinkHref="#robot" />
+        </svg>
+        <StyledCode>{code}</StyledCode>
+        <StyledDescription>{getErrorMessage(code)}</StyledDescription>
+        <Button>
+          Refresh
+        </Button>
+      </StyledWrapper>
+    </Container>
   )
 }
 
