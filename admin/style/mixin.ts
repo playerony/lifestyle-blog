95d4f6e2a2d9b90d1a7b3b178f9a95b5b1b3d7ef
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-19 16:56:52 +0100

    Cleanup mixins

--- admin/style/mixin.ts
+++ admin/style/mixin.ts
@@ -3,12 +3,14 @@ import { css, FlattenSimpleInterpolation } from 'styled-components'
 import variable from './variable'
 import { IBreakpoint } from './variable/variable.type'
 
-export const transition = (argument: string): FlattenSimpleInterpolation => css`
-  -webkit-transition: ${argument};
-  -moz-transition: ${argument};
-  -ms-transition: ${argument};
-  -o-transition: ${argument};
-  transition: ${argument};
+export const transition = (
+  transitions: string
+): FlattenSimpleInterpolation => css`
+  transition: ${transitions};
+  -o-transition: ${transitions};
+  -ms-transition: ${transitions};
+  -moz-transition: ${transitions};
+  -webkit-transition: ${transitions};
 `
 
 export const linearGradient = (
@@ -19,50 +21,50 @@ export const linearGradient = (
 
   return css`
     background: nth(nth(${formattedColorSteps}, 1), 1);
+    background: linear-gradient(${direction}, ${formattedColorSteps});
     background: -webkit-linear-gradient(
       legacy-direction(${direction}),
       ${formattedColorSteps}
     );
-    background: linear-gradient(${direction}, ${formattedColorSteps});
   `
 }
 
 export const transform = (
   transforms: string
 ): FlattenSimpleInterpolation => css`
-  -webkit-transform: ${transforms};
-  -moz-transform: ${transforms};
-  -ms-transform: ${transforms};
-  -o-transform: ${transforms};
   transform: ${transforms};
+  -o-transform: ${transforms};
+  -ms-transform: ${transforms};
+  -moz-transform: ${transforms};
+  -webkit-transform: ${transforms};
 `
 
 export const transformStyle = (
   style: string
 ): FlattenSimpleInterpolation => css`
-  -webkit-transform-style: ${style};
-  -moz-transform-style: ${style};
-  -ms-transform-style: ${style};
-  -o-transform-style: ${style};
   transform-style: ${style};
+  -o-transform-style: ${style};
+  -ms-transform-style: ${style};
+  -moz-transform-style: ${style};
+  -webkit-transform-style: ${style};
 `
 
 export const transformOrigin = (
   origin: string
 ): FlattenSimpleInterpolation => css`
-  -webkit-transform-origin: ${origin};
-  -moz-transform-origin: ${origin};
-  -ms-transform-origin: ${origin};
-  -o-transform-origin: ${origin};
   transform-origin: ${origin};
+  -o-transform-origin: ${origin};
+  -ms-transform-origin: ${origin};
+  -moz-transform-origin: ${origin};
+  -webkit-transform-origin: ${origin};
 `
 
 export const animation = (animation: string): FlattenSimpleInterpolation => css`
-  -webkit-animation: ${animation};
-  -moz-animation: ${animation};
-  -ms-animation: ${animation};
-  -o-animation: ${animation};
   animation: ${animation};
+  -o-animation: ${animation};
+  -ms-animation: ${animation};
+  -moz-animation: ${animation};
+  -webkit-animation: ${animation};
 `
 
 export const borderRadius = (radius: string): FlattenSimpleInterpolation => css`
@@ -72,58 +74,58 @@ export const borderRadius = (radius: string): FlattenSimpleInterpolation => css`
 `
 
 export const flexbox = (): FlattenSimpleInterpolation => css`
-  display: -webkit-box;
+  display: flex;
   display: -moz-box;
+  display: -webkit-box;
   display: -ms-flexbox;
   display: -webkit-flex;
-  display: flex;
 `
 
 export const justifyContent = (
   justify: string
 ): FlattenSimpleInterpolation => css`
-  -webkit-justify-content: ${justify};
-  -moz-justify-content: ${justify};
-  -ms-justify-content: ${justify};
-  justify-content: ${justify};
   -ms-flex-pack: ${justify};
+  justify-content: ${justify};
+  -ms-justify-content: ${justify};
+  -moz-justify-content: ${justify};
+  -webkit-justify-content: ${justify};
 `
 
 export const alignItems = (align: string): FlattenSimpleInterpolation => css`
-  -webkit-align-items: ${align};
-  -moz-align-items: ${align};
-  -ms-align-items: ${align};
-  -ms-flex-align: ${align};
   align-items: ${align};
+  -ms-flex-align: ${align};
+  -ms-align-items: ${align};
+  -moz-align-items: ${align};
+  -webkit-align-items: ${align};
 `
 
 export const flexDirection = (
   direction: string
 ): FlattenSimpleInterpolation => css`
-  -webkit-flex-direction: ${direction};
-  -moz-flex-direction: ${direction};
-  -ms-flex-direction: ${direction};
   flex-direction: ${direction};
+  -ms-flex-direction: ${direction};
+  -moz-flex-direction: ${direction};
+  -webkit-flex-direction: ${direction};
 `
 
 export const flexWrap = (wrap: string): FlattenSimpleInterpolation => css`
-  -webkit-flex-wrap: ${wrap};
-  -moz-flex-wrap: ${wrap};
-  -ms-flex-wrap: ${wrap};
   flex-wrap: ${wrap};
+  -ms-flex-wrap: ${wrap};
+  -moz-flex-wrap: ${wrap};
+  -webkit-flex-wrap: ${wrap};
 `
 
 export const boxShadow = (shadow: string): FlattenSimpleInterpolation => css`
-  -webkit-box-shadow: ${shadow};
-  -moz-box-shadow: ${shadow};
   box-shadow: ${shadow};
+  -moz-box-shadow: ${shadow};
+  -webkit-box-shadow: ${shadow};
 `
 
 export const appearance = (value: string): FlattenSimpleInterpolation => css`
-  -webkit-appearance: ${value};
-  -moz-appearance: ${value};
-  -ms-appearance: ${value};
   appearance: ${value};
+  -ms-appearance: ${value};
+  -moz-appearance: ${value};
+  -webkit-appearance: ${value};
 `
 
 export const respondTo = (Object.keys(
