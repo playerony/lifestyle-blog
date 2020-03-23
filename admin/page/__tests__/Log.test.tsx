a63e1a7d55cc5370be572f5e0d03b9289c9c157c
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-23 16:21:52 +0100

    Log Page - tests

--- admin/page/__tests__/Log.test.tsx
+++ admin/page/__tests__/Log.test.tsx
@@ -1,43 +1,119 @@
-import React from 'react'
-import { shallow } from 'enzyme'
+import React, { ReactElement } from 'react'
+import { mount, ReactWrapper } from 'enzyme'
+import { MemoryRouter } from 'react-router-dom'
+import { MockedProvider, wait } from '@apollo/react-testing'
 
-import LogPage from '@component/LogPage'
+import { LOG_LIST_QUERY } from '@hook/log/useLogList/useLogList.query'
 
-import ILog from '@type/log/ILog'
+const useTitleMock = jest.fn()
+const toggleLoaderMock = jest.fn()
 
-jest.doMock('../../hook/log/useLogList', () => () => ({ data: LOG_LIST_MOCK, loading: false }))
+jest.doMock('../../component/LogPage', () => LogPageMock)
+jest.doMock('../../hook/utility/useTitle', () => useTitleMock)
+jest.doMock('../../hook/context/useLoader', () => () => ({ toggleLoader: toggleLoaderMock }))
+
+const mountComponent = (element: ReactElement): ReactWrapper =>
+  mount(
+    <MemoryRouter>
+      <MockedProvider mocks={LOG_LIST_QUERY_MOCK}>
+        {element}
+      </MockedProvider>
+    </MemoryRouter>
+  )
+
+const setUp = (): ReactWrapper => {
+  const Log = require('../Log').default
+
+  return mountComponent(<Log />)
+}
 
 describe('LogPage Page', () => {
+  beforeAll(() => {
+    console.error = jest.fn()
+  })
+
   it('should render', () => {
-    const Log = require('../Log').default
-    const wrapper = shallow(<Log />)
+    const wrapper = setUp()
 
     expect(wrapper.exists()).toBeTruthy()
   })
 
-  it('should render LogPage Component', () => {
-    const Log = require('../Log').default
-    const wrapper = shallow(<Log />)
+  it('should set a proper document title', () => {
+    setUp()
 
-    expect(wrapper.exists(LogPage)).toBeTruthy()
+    expect(useTitleMock).toHaveBeenCalledWith('Log')
   })
 
-  it('should render LogPage Component with proper data', () => {
-    const Log = require('../Log').default
-    const wrapper = shallow(<Log />)
+  describe('loading data', () => {
+    it('should return null during data loading', () => {
+      const wrapper = setUp()
 
-    const logPageProps = wrapper.find(LogPage).props()
+      expect(wrapper.html()).toEqual('')
+    })
 
-    expect(Object.keys(logPageProps)).toHaveLength(1)
-    expect(logPageProps.logList).toEqual(LOG_LIST_MOCK)
+    it('should render LogPage Component after data loading', async () => {
+      const wrapper = setUp()
+
+      await wait(0)
+      wrapper.update()
+
+      expect(wrapper.exists(LogPageMock)).toBeTruthy()
+    })
+
+    it('should render LogPage Component with proper prop', async () => {
+      const wrapper = setUp()
+
+      await wait(0)
+      wrapper.update()
+
+      expect((wrapper.find(LogPageMock).props() as any).logList).toEqual(LOG_LIST_RESULT_DATA.logList)
+    })
   })
+
+  it('should call toggleLoader function', () => {
+    setUp()
+
+    expect(toggleLoaderMock).toHaveBeenCalledWith(true)
+  })
+
+  // it('should render LogPage Component', () => {
+  //   const Log = require('../Log').default
+  //   const wrapper = shallow(<Log />)
+
+  //   expect(wrapper.exists(LogPage)).toBeTruthy()
+  // })
+
+  // it('should render LogPage Component with proper data', () => {
+  //   const Log = require('../Log').default
+  //   const wrapper = shallow(<Log />)
+
+  //   const logPageProps = wrapper.find(LogPage).props()
+
+  //   expect(Object.keys(logPageProps)).toHaveLength(1)
+  //   expect(logPageProps.logList).toEqual(LOG_LIST_MOCK)
+  // })
 })
 
-const LOG_LIST_MOCK: ILog[] = [
+const LogPageMock = (): JSX.Element => <h1>Log Page</h1>
+
+const LOG_LIST_RESULT_DATA = {
+  logList: [
+    {
+      createdAt: new Date('2020'),
+      __typename: 'Log'
+    },
+    {
+      createdAt: new Date('2019'),
+      __typename: 'Log'
+    }
+  ]
+}
+
+const LOG_LIST_QUERY_MOCK = [
   {
-    createdAt: new Date('2020')
-  },
-  {
-    createdAt: new Date('2019')
+    request: {
+      query: LOG_LIST_QUERY
+    },
+    result: { data: LOG_LIST_RESULT_DATA }
   }
 ]
