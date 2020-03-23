a74dcc3fede553e7f0e79fdacc9cfa579cedc6b8
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-23 16:24:07 +0100

    Loading Page - tests

--- admin/page/__tests__/Loading.test.tsx
+++ admin/page/__tests__/Loading.test.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { mount, ReactWrapper } from 'enzyme'
 
 import LoadingPage from '../Loading'
 import Loader from '@component/common/Loader'
 import ThemeProviderMock from '@component/utility/ThemeProviderMock'
 
+const setUp = (): ReactWrapper =>
+  mount(<ThemeProviderMock><LoadingPage /></ThemeProviderMock>)
+
 describe('Loading Page', () => {
   it('should render', () => {
-    const wrapper = mount(<ThemeProviderMock><LoadingPage /></ThemeProviderMock>)
+    const wrapper = setUp()
 
-    expect(wrapper).toBeDefined()
+    expect(wrapper.exists()).toBeTruthy()
     expect(wrapper.find(Loader).length).toEqual(1)
+  })
+
+  it('should render Loader Component', () => {
+    const wrapper = setUp()
+
+    expect(wrapper.exists()).toBeTruthy()
+    expect(wrapper.exists(Loader)).toBeTruthy()
   })
 })
