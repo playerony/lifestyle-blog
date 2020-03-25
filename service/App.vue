e8502737abde4c3c6466180e96b6a87c1c4b6487
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-25 19:32:15 +0100

    Refactor base Vue app

---
+++ service/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="root">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@component/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
