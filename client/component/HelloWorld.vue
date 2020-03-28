dbf30be44e144e99a2a12f68765892b46abaf717
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-28 11:48:59 +0100

    Simple switch to change theme mode

--- client/component/HelloWorld.vue
+++ client/component/HelloWorld.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="hello">
     <p>
-      Hello World my frind
+      Hello World my friend
     </p>
+    <input type="checkbox" v-model="darkMode"/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld'
+  name: 'HelloWorld',
+  data() {
+    return {
+      darkMode: false
+    }
+  },
+  watch: {
+    darkMode: function () {
+      let htmlElement = document.documentElement
+
+      if (this.darkMode) {
+        htmlElement.setAttribute('theme', 'dark')
+      } else {
+        htmlElement.setAttribute('theme', 'light')
+      }
+    }
+  }
 }
 </script>
