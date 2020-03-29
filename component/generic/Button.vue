f1cb61ccd5b22cb3ce897a28362b01e27989d6c4
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-29 13:23:03 +0200

    Create generic button component

---
+++ component/generic/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button
+      class="button"
+      @click="onClick"
+      :disabled="disabled" 
+    >
+      <slot>button</slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    onClick: {
+      type: Function,
+      required: true
+    },
+    disabled: {
+      type: Boolean
+    }
+  }
+}
+</script>
