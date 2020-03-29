b6291e12f4cd452757bf0f23636aa75d978d079b
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-29 13:23:03 +0200

    Create generic button component

--- client/component/HelloWorld.vue
+++ client/component/HelloWorld.vue
@@ -4,12 +4,25 @@
       Hello World my friend reload
     </p>
     <input type="checkbox" v-model="darkMode"/>
+    <Button :onClick="consoleClick">
+      <h1>Example button</h1>
+    </Button>
   </div>
 </template>
 
 <script>
+import Button from './generic/Button'
+
 export default {
   name: 'HelloWorld',
+  components: {
+    Button
+  },
+  methods: {
+    consoleClick() {
+      console.log('Button clicked')
+    }
+  },
   data() {
     return {
       darkMode: false
