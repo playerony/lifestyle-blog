a44a4dfeed778c8b4a0ed39942c9bf2f95ac10bc
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-30 17:03:03 +0200

    Create generic switch component

---
+++ component/generic/Checkbox.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    :class="'switch ' + swichStyle"
+    @click="$emit('click', !isChecked)"
+  >
+    <div class="switch__circle" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Checkbox',
+  props: {
+    isChecked: { type: Boolean, required: false, default: false }
+  },
+  computed: {
+    swichStyle() {
+      if (this.isChecked) {
+        return 'switch--checked'
+      }
+
+      return ''
+    }
+  },
+  model: {
+    event: "click",
+    prop: "isChecked"
+  }
+}
+</script>
