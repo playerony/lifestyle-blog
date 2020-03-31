3dab6f73f6c4305471319f1d3d3b18141fee8fca
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-03-31 16:10:36 +0200

    Create tests for Button generic component

--- client/component/generic/__tests__/Button.spec.js
+++ client/component/generic/__tests__/Button.spec.js
@@ -1,10 +1,42 @@
 import { mount } from '@vue/test-utils'
+
 import Button from '../Button'
 
+const setUp = (propsData) =>
+  mount(Button, { propsData })
+
 describe('Button Component', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Button)
+  it('should render', () => {
+    const wrapper = setUp({ onClick: jest.fn() })
+
+    expect(wrapper.exists()).toBeTruthy()
+  })
+
+  it('is a Vue instance', () => {
+    const wrapper = setUp({ onClick: jest.fn() })
 
     expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('should contain optional disabled property with default value', () => {
+    const wrapper = setUp({ onClick: jest.fn() })
+
+    expect(wrapper.props().disabled).toEqual(false)
+  })
+
+  describe('button element', () => {
+    it('should render', () => {
+      const wrapper = setUp({ onClick: jest.fn() })
+
+      expect(wrapper.exists('button')).toBeTruthy()
+    })
+
+    it('should trigger onClick function', () => {
+      const onClickMock = jest.fn()
+      const wrapper = setUp({ onClick: onClickMock })
+
+      wrapper.find('button').trigger('click')
+      expect(onClickMock).toHaveBeenCalled()
+    })
   })
 })
