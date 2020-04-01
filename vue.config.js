490e0b74957514c3ef84343f0d5ff5c7a899b802
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-01 21:12:42 +0200

    Update project paths + remove class prop from slider

--- vue.config.js
+++ vue.config.js
@@ -16,6 +16,7 @@ module.exports = {
         '@style': path.resolve(__dirname, 'style/'),
         '@config': path.resolve(__dirname, 'config/'),
         '@service': path.resolve(__dirname, 'service/'),
+        '@utility': path.resolve(__dirname, 'utility/'),
         '@component': path.resolve(__dirname, 'component/')
       }
     },
