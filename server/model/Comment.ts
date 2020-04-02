eeb2181a770566402c542b5f973b1be8972baea5
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-02 16:22:23 +0200

    Create Comment table model

---
+++ server/model/Comment.ts
@@ -0,0 +1,38 @@
+import { Model, DataTypes } from 'sequelize'
+
+import { sequelize } from '@config/database/sequelize'
+
+import { Article } from './Article'
+
+export class Comment extends Model {}
+
+Comment.init(
+  {
+    commentId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    parentCommentId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: null
+    },
+    articleId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    creator: {
+      type: DataTypes.STRING(20),
+      allowNull: false
+    }
+  },
+  { sequelize }
+)
+
+Comment.belongsTo(Article, { as: 'article', foreignKey: 'articleId' })
+Comment.belongsTo(Comment, { as: 'parentComment', foreignKey: 'parentCommentId' })
