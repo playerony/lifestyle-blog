f808b889546bd2a0f1d4f15355d65aaab529686a
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-02 16:25:31 +0200

    Create tests for Comment model

---
+++ server/model/__tests__/Comment.test.ts
@@ -0,0 +1,55 @@
+// @ts-nocheck
+const { dataTypes: DataTypes } = require('sequelize-test-helpers')
+
+class Sequelize {}
+
+const initMock = jest.fn()
+
+jest.doMock('sequelize', () => {
+  class Model {
+    public static init = initMock
+    public static belongsTo = jest.fn()
+  }
+
+  return {
+    Model,
+    DataTypes,
+    Sequelize
+  }
+})
+
+describe('Comment Model', () => {
+  it('should call Comment.init with the correct parameters', () => {
+    require('../Comment')
+
+    expect(initMock).toBeCalledWith(
+      {
+        commentId: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        parentCommentId: {
+          type: DataTypes.INTEGER,
+          allowNull: true,
+          defaultValue: null
+        },
+        articleId: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+        },
+        content: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        },
+        creator: {
+          type: DataTypes.STRING(20),
+          allowNull: false
+        }
+      },
+      {
+        sequelize: new Sequelize()
+      }
+    )
+  })
+})
