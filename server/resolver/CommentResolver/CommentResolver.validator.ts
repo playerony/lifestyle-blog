4a47874d18b6bb268fac3749ec37ca0788d05eee
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-03 18:39:59 +0200

    Create validators for comment resolver

---
+++ server/resolver/CommentResolver/CommentResolver.validator.ts
@@ -0,0 +1,72 @@
+import { CommentSaveRequest } from '@type/Comment'
+
+import ValidationError from '@utility/ValidationError'
+import { check, validate, isValid } from '@utility/validate'
+
+export const createCommentValidation = (data: CommentSaveRequest): void => {
+  const validationResult = validate(data)
+    .setCheckList([
+      check('parentCommentId')
+        .isExist()
+        .isNumber(),
+      check('articleId')
+        .isExist()
+        .isNumber(),
+      check('content')
+        .isExist()
+        .isMinLength(3),
+      check('creator')
+        .isExist()
+        .isMinLength(2)
+        .isMaxLength(20)
+    ])
+    .check()
+
+  if (!isValid(validationResult)) {
+    throw new ValidationError(validationResult)
+  }
+}
+
+export const updateCommentValidation = (
+  commentId: number,
+  data: CommentSaveRequest
+): void => {
+  const validationResult = validate({ commentId, ...data })
+    .setCheckList([
+      check('commentId')
+        .isExist()
+        .isNumber(),
+      check('parentCommentId')
+        .isExist()
+        .isNumber(),
+      check('articleId')
+        .isExist()
+        .isNumber(),
+      check('content')
+        .isExist()
+        .isMinLength(3),
+      check('creator')
+        .isExist()
+        .isMinLength(2)
+        .isMaxLength(20)
+    ])
+    .check()
+
+  if (!isValid(validationResult)) {
+    throw new ValidationError(validationResult)
+  }
+}
+
+export const commentListByArticleIdValidation = (articleId: number): void => {
+  const validationResult = validate({ articleId })
+    .setCheckList([
+      check('articleId')
+        .isExist()
+        .isNumber()
+    ])
+    .check()
+
+  if (!isValid(validationResult)) {
+    throw new ValidationError(validationResult)
+  }
+}
--- server/resolver/CommentResolver/index.ts
+++ server/resolver/CommentResolver/index.ts
@@ -4,6 +4,12 @@ import CommentService from '@service/CommentService'
 
 import { CommentType, CommentSaveRequest } from '@type/Comment'
 
+import {
+  createCommentValidation,
+  updateCommentValidation,
+  commentListByArticleIdValidation
+} from './CommentResolver.validator'
+
 @Resolver()
 export default class CommentResolver {
   constructor(private commentService: CommentService = new CommentService()) {}
@@ -12,6 +18,8 @@ export default class CommentResolver {
   async commentListByArticleId(
     @Arg('articleId', type => Int) articleId: number
   ): Promise<CommentType[]> {
+    commentListByArticleIdValidation(articleId)
+
     return this.commentService.findAllByArticleId(articleId)
   }
 
@@ -19,6 +27,8 @@ export default class CommentResolver {
   async createComment(
     @Arg('comment', type => CommentSaveRequest) comment: CommentSaveRequest
   ): Promise<CommentType> {
+    createCommentValidation(comment)
+
     return this.commentService.create(comment)
   }
 
@@ -27,6 +37,8 @@ export default class CommentResolver {
     @Arg('commentId', type => Int) commentId: number,
     @Arg('comment', type => CommentSaveRequest) comment: CommentSaveRequest
   ): Promise<CommentType | null> {
+    updateCommentValidation(commentId, comment)
+
     return this.commentService.update(commentId, comment)
   }
 }
