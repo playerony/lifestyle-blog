211bc834edea5f47b012941c61417ad0c9ecc48f
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-05 15:52:27 +0200

    Create Loader component

---
+++ client/component/common/Loader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="loader">
+    <div class="box">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div class="box">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div class="box">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div class="box">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>    
+</template>
+
+<script>
+export default {
+  name: 'Loader'
+}
+</script>
--- client/page/Home.vue
+++ client/page/Home.vue
@@ -1,12 +1,21 @@
 <template>
-  <h1>Home</h1>
+  <Container>
+    <Loader />
+  </Container>
 </template>
 
 <script>
+import Loader from '@component/common/Loader'
+import Container from '@component/common/Container'
+
 import { ARTICLE_LIST_QUERY } from '@graphql/query/articleList'
 
 export default {
   name: 'Home',
+  components: {
+    Loader,
+    Container
+  },
   data () {
     return {
       articleList: []
