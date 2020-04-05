79b3bde5878bb4e7754a4a5eda356bb777af8e42
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-05 14:03:02 +0200

    Create articleList query

--- client/page/Home.vue
+++ client/page/Home.vue
@@ -1,3 +1,21 @@
 <template>
   <h1>Home</h1>
 </template>
+
+<script>
+import { ARTICLE_LIST_QUERY } from '@graphql/query/articleList'
+
+export default {
+    name: 'Users',
+    data () {
+      return {
+        articleList: []
+      }
+    },
+    apollo: {
+      articleList: {
+        query: ARTICLE_LIST_QUERY
+      }
+    }
+}
+</script>
