0cf48bd9d93e9c62ff14b6513ca84d2f006f8aa6
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-05 14:38:28 +0200

    Create tests for routeList file

---
+++ config/__tests__/routeList.spec.js
@@ -0,0 +1,17 @@
+import routeList from '../routeList'
+
+describe('routeList Configuration', () => {
+  it('should import', () => {
+    expect(typeof routeList).toEqual('object')
+  })
+
+  it('should return proper object', () => {
+    expect(routeList).toEqual(ROUTE_LIST)
+  })
+})
+
+const ROUTE_LIST = {
+  base: '/',
+  article: '/article/:articleId',
+  articles: '/articles/:categoryId'
+}
