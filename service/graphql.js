677789c5e7c803e70a751d30e1a248e2ccaf1bd4
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-05 14:26:16 +0200

    Configure enviroment variables

--- service/graphql.js
+++ service/graphql.js
@@ -4,10 +4,12 @@ import { HttpLink } from 'apollo-link-http'
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
+import keys from '@config/keys'
+
 Vue.use(VueApollo)
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:3100/graphql'
+  uri: `${keys.serverUrl}/graphql`
 })
 
 const apolloClient = new ApolloClient({
