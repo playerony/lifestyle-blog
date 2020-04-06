073e58d7788059b0ccedc8850f25a2c261a69202
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-06 17:05:50 +0200

    Create loading page

---
+++ component/Home/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img :src="article.image.photoUrl" />
+    <h1>{{ article.title }}</h1>
+    <h2>{{ article.description }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+  props: {
+    article: { type: Object, required: true }
+  }
+}
+</script>
