d4c27a3e96951a050d1699419bb885d30c9d55b3
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-09 20:57:03 +0200

    Create jest mock for assets

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,9 @@ module.exports = {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest'
   },
   moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/config/fileMock.js',
+
     '@page/(.*)': '<rootDir>/page/$1',
     '@style/(.*)': '<rootDir>/style/$1',
     '@config/(.*)': '<rootDir>/config/$1',
