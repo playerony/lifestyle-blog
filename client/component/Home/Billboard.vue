94ccc8dae4f527c1729336aa5e68625a575a69ae
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-10 14:28:02 +0200

    Remove mask effect from billboard

--- client/component/Home/Billboard.vue
+++ client/component/Home/Billboard.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="billboard">
-    <div class="billboard__mask" />
-    <img class="billboard__image" :src="article.image.photoUrl" />
+  <div class="billboard" @mousemove="handleMouseMove">
+    <img
+      class="billboard__image"
+      id="billboard-background"
+      :src="article.image.photoUrl" />
     <section class="billboard__content">
       <h2 class="content__header">Newest</h2>
       <h1 class="content__title">{{ article.title }}</h1>
@@ -26,6 +28,20 @@ export default {
   data () {
     return {
       date: formatArticleDate(this.article.createdAt)
+    }
+  },
+  methods: {
+    handleMouseMove(event) {
+      event.preventDefault()
+
+      const billboardImage = document.getElementById('billboard-background')
+      if (billboardImage) {
+        const billboardOriginX = billboardImage.clientWidth / 2
+        const billboardOriginY = billboardImage.clientHeight / 2
+
+        billboardImage.style.left = (billboardOriginX - event.screenX) * 0.02 - 10 + 'px'
+        billboardImage.style.top = (billboardOriginY - event.screenY) * 0.02 - 10 + 'px'
+      }
     }
   },
   props: {
