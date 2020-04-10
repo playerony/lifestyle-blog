41f4b294a1de7154a7b25050b39f5be93ab80525
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-10 18:35:11 +0200

    Remove text selection

--- client/component/Home/Billboard.vue
+++ client/component/Home/Billboard.vue
@@ -3,20 +3,22 @@
     <img
       class="billboard__image"
       id="billboard-background"
-      :src="article.image.photoUrl" />
+      :src="article.image.photoUrl"
+    />
     <section class="billboard__content">
       <h2 class="content__header">Newest</h2>
       <h1 class="content__title">{{ article.title }}</h1>
       <h2 class="content__description">{{ article.description }}</h2>
     </section>
-    <section class="billboard__links">
-      <router-link :to="/article/ + article.articleId">
-        READ
-      </router-link>
-    </section>
-    <section class="billboard__date">
-      <h3>{{ date }}</h3>
-    </section>
+    <router-link
+      class="billboard__link"
+      :to="/article/ + article.articleId"
+    >
+      READ
+    </router-link>
+    <h3 class="billboard__date">
+      {{ getDate() }}
+    </h3>
   </div>
 </template>
 
@@ -25,12 +27,10 @@ import formatArticleDate from '@utility/formatArticleDate'
 
 export default {
   name: 'Billboard',
-  data () {
-    return {
-      date: formatArticleDate(this.article.createdAt)
-    }
-  },
   methods: {
+    getDate () {
+      return formatArticleDate(this.article.createdAt)
+    },
     handleMouseMove(event) {
       const billboardImage = document.getElementById('billboard-background')
 
