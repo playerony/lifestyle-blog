ccdb1faa7ecfdcd0716833315770af0f340e2a27
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-10 10:50:53 +0200

    Create tests for ArticleCard component

--- client/component/common/__tests__/ArticleCard.spec.js
+++ client/component/common/__tests__/ArticleCard.spec.js
@@ -5,6 +5,10 @@ import ArticleCard from '../ArticleCard'
 const setUp = () => mount(ArticleCard, { propsData: { article: ARTICLE_MOCK } })
 
 describe('ArticleCard Component', () => {
+  beforeAll(() => {
+    console.error = jest.fn()
+  })
+
   it('should render', () => {
     const wrapper = setUp()
 
@@ -15,6 +19,52 @@ describe('ArticleCard Component', () => {
     const wrapper = setUp()
 
     expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('should we wrapped in router-link', () => {
+    const wrapper = setUp()
+
+    expect(wrapper.find('router-link').attributes()).toEqual({
+      class: 'article-card',
+      to: '/article/7'
+    })
+  })
+
+  it('should render image attached to the article as a background', () => {
+    const wrapper = setUp()
+
+    expect(wrapper.find('img').attributes()).toEqual({
+      class: 'article-card__image',
+      src: 'photoUrl'
+    })
+  })
+
+  it('should render reading time', () => {
+    const wrapper = setUp()
+
+    expect(wrapper.find('p').text()).toEqual('1 min')
+  })
+
+  it('should render well formatted article creation date', () => {
+    const wrapper = setUp()
+
+    expect(
+      wrapper
+        .findAll('h4')
+        .at(1)
+        .text()
+    ).toEqual('Jan 01, 2020')
+  })
+
+  it('should render article title and description', () => {
+    const wrapper = setUp()
+
+    expect(
+      wrapper
+        .findAll('section')
+        .at(0)
+        .text()
+    ).toEqual('title description')
   })
 })
 
