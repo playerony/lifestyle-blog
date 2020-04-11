012ae40a78bafeb056ec2aabb8d6936b1984c1e7
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-11 17:03:29 +0200

    Fetch article data by id

--- graphql/query/article.js
+++ graphql/query/article.js
@@ -1,9 +1,14 @@
 import gql from 'graphql-tag'
 
-export const ARTICLE_QUERY = gql`
+const ARTICLE_QUERY = gql`
   query ArticleById($articleId: Int) {
     articleById(articleId: $articleId) {
       content
     }
   }
 `
+
+export default {
+  query: ARTICLE_QUERY,
+  fetchPolicy: 'cache-first'
+}
