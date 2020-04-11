6938540dcadeb5a267cc19a89e12b23e6dea9bae
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-11 17:21:31 +0200

    Display fetched article content

---
+++ component/Article/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-html="content" />
+</template>
+
+<script>
+import draftToHtml from 'draftjs-to-html';
+
+export default {
+  name: 'Article',
+  props: {
+    article: { type: Object, required: true }
+  },
+  data () {
+    return {
+      content: draftToHtml(JSON.parse(this.article.content))
+    }
+  },
+}
+</script>
