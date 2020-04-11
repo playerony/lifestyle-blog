b4eebf5dcf610732147f5926d909bda40915e264
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-11 17:03:29 +0200

    Fetch article data by id

--- client/page/Article.vue
+++ client/page/Article.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>Article</h1>
+  <fragment>
+    Article
+  </fragment>
 </template>
+
+<script>
+import articleQuery from '@graphql/query/article'
+
+export default {
+  name: 'ArticlePage',
+  data () {
+    return {
+      articleById: {},
+      routeParams: this.$route.params
+    }
+  },
+  apollo: {
+    articleById: {
+      ...articleQuery,
+      variables () {
+        return {
+          articleId: Number(this.routeParams.articleId)
+        }
+      }
+    }
+  }
+}
+</script>
