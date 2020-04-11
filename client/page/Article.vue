ddda06b5d9bfad2120c0be6c80d560ce38a6ac9c
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-11 17:21:31 +0200

    Display fetched article content

--- client/page/Article.vue
+++ client/page/Article.vue
@@ -1,14 +1,22 @@
 <template>
   <fragment>
-    Article
+    <LoadingPage v-if="$apollo.queries.articleById.loading" />
+    <Article v-if="!$apollo.queries.articleById.loading" :article="articleById" />
   </fragment>
 </template>
 
 <script>
+import LoadingPage from './Loading'
+import Article from '@component/Article'
+
 import articleQuery from '@graphql/query/article'
 
 export default {
   name: 'ArticlePage',
+  components: {
+    Article,
+    LoadingPage
+  },
   data () {
     return {
       articleById: {},
