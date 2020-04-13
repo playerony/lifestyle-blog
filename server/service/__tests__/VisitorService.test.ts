fc3120f347cc93d013522ffdbad91c15a00a5eaf
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-13 14:11:19 +0200

    Update tests for the visitor service

--- server/service/__tests__/VisitorService.test.ts
+++ server/service/__tests__/VisitorService.test.ts
@@ -1,5 +1,7 @@
 const { dataTypes: DataTypes } = require('sequelize-test-helpers')
 
+import VisitorService from '../VisitorService'
+
 import { VisitorType } from '@type/Visitor'
 
 let foundVisitorMock = jest.fn().mockImplementation(() => VISITOR_MOCK)
@@ -16,6 +18,7 @@ const setupSequelizeMock = (
       public static init = jest.fn()
       public static findOne = foundVisitor
       public static create = createVisitor
+      public static findAll = jest.fn().mockImplementation(() => [VISITOR_MOCK])
     }
 
     return {
@@ -25,7 +28,13 @@ const setupSequelizeMock = (
     }
   })
 
-describe('VisitorService Service', () => {
+const setUp = (): VisitorService => {
+  const VisitorService = require('../VisitorService').default
+
+  return new VisitorService()
+}
+
+describe('Visitor Service', () => {
   beforeEach(() => {
     jest.resetModules()
   })
@@ -34,8 +43,7 @@ describe('VisitorService Service', () => {
     it('should return null value', async () => {
       setupSequelizeMock()
 
-      const VisitorService = require('../VisitorService').default
-      const visitorService = new VisitorService()
+      const visitorService = setUp()
 
       const result = await visitorService.create(
         VISITOR_MOCK.articleId,
@@ -48,8 +56,7 @@ describe('VisitorService Service', () => {
     it('should return created visitor value', async () => {
       setupSequelizeMock(jest.fn().mockImplementation(() => undefined))
 
-      const VisitorService = require('../VisitorService').default
-      const visitorService = new VisitorService()
+      const visitorService = setUp()
 
       const result = await visitorService.create(
         VISITOR_MOCK.articleId,
@@ -59,11 +66,23 @@ describe('VisitorService Service', () => {
       expect(result).toEqual(VISITOR_MOCK)
     })
   })
+
+  describe('findAll Method', () => {
+    it('should return found visitor list', async () => {
+      const articleService = setUp()
+
+      const result = await articleService.findAll()
+
+      expect(result).toEqual([VISITOR_MOCK])
+    })
+  })
 })
 
-const VISITOR_MOCK: VisitorType = {
+const VISITOR_MOCK: Required<VisitorType> = {
   visitorId: 1,
   articleId: 1,
   userAgent: 'Mobile',
-  ipAddress: '192.192.192.192'
+  ipAddress: '192.192.192.192',
+  createdAt: new Date('2020'),
+  updatedAt: new Date('2020')
 }
