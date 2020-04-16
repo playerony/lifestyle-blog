921eb54c2d66be851c82c167b522bff8a71010ce
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-16 21:18:31 +0200

    Create tests for the article query

---
+++ client/graphql/query/__tests__/article.spec.js
@@ -0,0 +1,24 @@
+import gql from 'graphql-tag'
+
+import articleQuery from '../article'
+
+describe('article query', () => {
+  it('should import', () => {
+    expect(typeof articleQuery).toBeDefined()
+  })
+
+  it('should contain proper query and fetch policy', () => {
+    expect(articleQuery).toEqual({
+      query: QUERY_FORMULA,
+      fetchPolicy: 'cache-first'
+    })
+  })
+})
+
+const QUERY_FORMULA = gql`
+  query ArticleById($articleId: Int) {
+    articleById(articleId: $articleId) {
+      content
+    }
+  }
+`
