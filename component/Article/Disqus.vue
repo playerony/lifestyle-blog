1417c200d4ab8b3650045b90fc8b9c0db8d5c99c
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-16 17:24:18 +0200

    Render recursive all article comments

--- component/Article/Disqus.vue
+++ component/Article/Disqus.vue
@@ -1,20 +1,35 @@
 <template>
   <div>
     <Reply :handleReply="handleReply" />
+    <Comment
+      v-for="comment in getCommentList()"
+      :key="comment.commentId"
+      :comment="comment"
+      :handleReply="handleReply"
+    />
   </div>
 </template>
 
 <script>
 import Reply from './Reply'
+import Comment from './Comment'
+
+import commentListTransformator from '@transformator/commentListTransformator'
 
 export default {
   name: 'Disqus',
   components: {
-    Reply
+    Reply,
+    Comment
   },
   props: {
     comments: { type: Array, required: true },
     handleReply: { type: Function, required: true }
+  },
+  methods: {
+    getCommentList() {
+      return commentListTransformator(this.comments)
+    }
   }
 }
 </script>
