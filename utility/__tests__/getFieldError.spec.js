1e05108405098b9ef80a2bc0b85ed00f922dba54
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-18 17:59:55 +0200

    Create tests for the getFieldError function

---
+++ utility/__tests__/getFieldError.spec.js
@@ -0,0 +1,31 @@
+import getFieldError from '../getFieldError'
+
+describe('getFieldError function', () => {
+  it('import getFieldError', () => {
+    expect(typeof getFieldError).toEqual('function')
+  })
+
+  it('should return empty string when passed error data object is not defined', () => {
+    expect(getFieldError(undefined, 'test1')).toEqual('')
+  })
+
+  it('should return empty string when passed object key is not defined', () => {
+    expect(getFieldError(ERROR_DATA_MOCK, 'test1')).toEqual('')
+    expect(getFieldError(ERROR_DATA_MOCK, 'test4')).toEqual('')
+  })
+
+  it('should return value when passed object key exists', () => {
+    expect(getFieldError(ERROR_DATA_MOCK, 'test2')).toEqual('Test2')
+  })
+
+  it('should return first value from array', () => {
+    expect(getFieldError(ERROR_DATA_MOCK, 'test3')).toEqual('Test1')
+  })
+})
+
+const ERROR_DATA_MOCK = {
+  test4: 1,
+  test1: [],
+  test2: ['Test2'],
+  test3: ['Test1', 'Test2']
+}
