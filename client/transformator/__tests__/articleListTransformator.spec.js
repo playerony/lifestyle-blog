4c2d8691aa21926829ef5735d75dc64482e48947
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-19 21:08:53 +0200

    Update tests for the articleList transformator

--- client/transformator/__tests__/articleListTransformator.spec.js
+++ client/transformator/__tests__/articleListTransformator.spec.js
@@ -6,17 +6,33 @@ describe('articleListTransformator Function', () => {
   })
 
   it('should return empty array if passed params are undefined', () => {
-    expect(articleListTransformator(undefined, undefined)).toEqual([])
+    expect(articleListTransformator(undefined, undefined, undefined)).toEqual(
+      []
+    )
   })
 
-  it('should proper calculate amount of total visitors', () => {
+  it('should proper calculate amount of total and todays visitors', () => {
     const result = articleListTransformator(
       ARTICLE_LIST_MOCK,
-      VISITOR_LIST_MOCK
+      VISITOR_LIST_MOCK,
+      COMMENT_LIST_MOCK
     )
 
     expect(result).toHaveLength(1)
-    expect(result[0].totalVisitor).toEqual(1)
+    expect(result[0].totalVisitors).toEqual(1)
+    expect(result[0].todayVisitors).toEqual(1)
+  })
+
+  it('should proper calculate amount of total and todays comments', () => {
+    const result = articleListTransformator(
+      ARTICLE_LIST_MOCK,
+      VISITOR_LIST_MOCK,
+      COMMENT_LIST_MOCK
+    )
+
+    expect(result).toHaveLength(1)
+    expect(result[0].totalComments).toEqual(5)
+    expect(result[0].todayComments).toEqual(3)
   })
 })
 
@@ -44,6 +60,40 @@ const VISITOR_LIST_MOCK = [
     createdAt: new Date()
   },
   {
+    createdAt: new Date()
+  },
+  {
+    createdAt: new Date()
+  },
+  {
+    articleId: 6,
+    createdAt: new Date('2017')
+  },
+  {
+    articleId: 7,
+    createdAt: new Date('2018')
+  },
+  {
+    articleId: 7,
+    createdAt: new Date('2019')
+  },
+  {
+    articleId: 2,
+    createdAt: new Date()
+  }
+]
+
+const COMMENT_LIST_MOCK = [
+  {
+    articleId: 7,
+    createdAt: new Date()
+  },
+  {
+    articleId: 7,
+    createdAt: new Date()
+  },
+  {
+    articleId: 7,
     createdAt: new Date()
   },
   {
