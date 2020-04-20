8872e56833a1a451ad58429fe78c9d6bb7b3f705
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-20 17:31:29 +0200

    Cleanup billboard component

--- client/component/Home/Billboard.vue
+++ client/component/Home/Billboard.vue
@@ -2,9 +2,29 @@
   <div class="billboard">
     <div class="billboard__border" />
     <section class="billboard__content">
+      <header class="content__header">
+        <p class="header__info">
+          <EyeSVG />
+          {{ article.totalVisitors }}
+          <span v-if="article.todayVisitors">
+            (+{{ article.todayVisitors }})
+          </span>
+        </p>
+        <p class="header__info">
+          <ClockSVG />
+          {{ article.readingTime }} min
+        </p>
+        <p class="header__info">
+          <ReplySVG />
+          {{ article.totalComments }}
+          <span v-if="article.todayComments">
+            (+{{ article.todayComments }})
+          </span>
+        </p>
+      </header>
       <h1 class="content__title">{{ article.title }}</h1>
       <label class="content__description">{{ article.description }}</label>
-      <Button>Read now</Button>
+      <Button className="content__button">Read now</Button>
     </section>
     <img class="billboard__image" :src="article.image.photoUrl" />
   </div>
@@ -12,6 +32,9 @@
 
 <script>
 import Button from '../generic/Button'
+import EyeSVG from '@asset/svg/eye.svg'
+import ClockSVG from '@asset/svg/clock.svg'
+import ReplySVG from '@asset/svg/reply.svg'
 
 export default {
   name: 'Billboard',
@@ -19,7 +42,10 @@ export default {
     article: { type: Object, required: true }
   },
   components: {
-    Button
+    Button,
+    EyeSVG,
+    ClockSVG,
+    ReplySVG
   }
 }
 </script>
