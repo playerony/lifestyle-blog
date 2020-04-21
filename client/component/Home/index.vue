756cfc87e80d320a246c08eb03f2a934e1d18fe6
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-21 20:45:15 +0200

    Remove unused components

--- client/component/Home/index.vue
+++ client/component/Home/index.vue
@@ -1,23 +1,16 @@
 <template>
   <fragment>
     <Billboard :article="getLastArticle()" />
-    <Container>
-      <Dashboard :articles="articles" />
-    </Container>
   </fragment>
 </template>
 
 <script>
 import Billboard from './Billboard'
-import Dashboard from './Dashboard'
-import Container from '../common/Container'
 
 export default {
   name: 'Home',
   components: {
-    Billboard,
-    Dashboard,
-    Container
+    Billboard
   },
   methods: {
     getLastArticle() {
