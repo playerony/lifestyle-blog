df8fed4a1014c1efee25e70b73dc3e4048007e8d
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-25 15:31:55 +0200

    Create navbar component

--- service/App.vue
+++ service/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="root" class="app">
+    <Navbar />
     <router-view />
   </div>
 </template>
 
 <script>
+import Navbar from '@component/common/Navbar'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar
+  }
 }
 </script>
