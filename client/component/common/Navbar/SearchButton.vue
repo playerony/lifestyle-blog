9ab8772272c736bee765fd0636f8e50aea18d965
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-27 16:06:09 +0200

    Create search blog button

---
+++ client/component/common/Navbar/SearchButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="search-button">
+    <label class="search-button__label">Search blog</label>
+    <SearchSVG class="search-button__icon" />
+  </div>
+</template>
+
+<script>
+import SearchSVG from '@asset/svg/search.svg'
+
+export default {
+  name: 'SearchButton',
+  components: {
+    SearchSVG
+  }
+}
+</script>
