981ec817ea30f89f319f58ceb3549a710fa6d1de
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-28 22:09:51 +0200

    Improve comments section styles

--- client/component/Article/Comment.vue
+++ client/component/Article/Comment.vue
@@ -6,13 +6,26 @@
         <p>{{ getCommentDate() }}</p>
       </header>
       <label>{{ comment.content }}</label>
+      <transition
+        name="accordion"
+        v-on:before-enter="beforeEnter"
+        v-on:enter="enter"
+        v-on:before-leave="beforeLeave"
+        v-on:leave="leave"
+      >
+        <Reply
+          v-show="isReplyVisible"
+          :handleReply="handleReply"
+          :errorData="getErrorData()"
+          :key="replyId"
+          :parentCommentId="comment.commentId"
+        />
+      </transition>
+      <LeftArrowSVG
+        :class="'content__expand-icon ' + getArrowStyle"
+        @click="isReplyVisible = !isReplyVisible"
+      />
     </div>
-    <Reply
-      :handleReply="handleReply"
-      :errorData="getErrorData()"
-      :key="'reply' + comment.commentId"
-      :parentCommentId="comment.commentId"
-    />
     <Comment
       v-for="comment in comment.comments"
       :key="comment.commentId"
@@ -28,15 +41,32 @@ import Comment from './Comment'
 
 import differenceBetweenDates from '@utility/differenceBetweenDates'
 
+import LeftArrowSVG from '@asset/svg/left-arrow.svg'
+
 export default {
   name: 'Comment',
   props: {
     comment: { type: Object, required: true },
     handleReply: { type: Function, required: true }
   },
+  data() {
+    return {
+      isReplyVisible: false
+    }
+  },
   components: {
     Reply,
-    Comment
+    Comment,
+    LeftArrowSVG
+  },
+  computed: {
+    getArrowStyle() {
+      if (this.isReplyVisible) {
+        return 'content__expand-icon--open'
+      }
+
+      return ''
+    }
   },
   methods: {
     getErrorData() {
@@ -44,6 +74,18 @@ export default {
     },
     getCommentDate() {
       return differenceBetweenDates(this.comment.createdAt)
+    },
+    beforeEnter: function(el) {
+      el.style.height = '0'
+    },
+    enter: function(el) {
+      el.style.height = el.scrollHeight + 'px'
+    },
+    beforeLeave: function(el) {
+      el.style.height = el.scrollHeight + 'px'
+    },
+    leave: function(el) {
+      el.style.height = '0'
     }
   }
 }
