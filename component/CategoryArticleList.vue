c213335f5cda4ab564eeb1986fe8c4745c4e8cf8
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-30 15:55:08 +0200

    Create category article list component

---
+++ component/CategoryArticleList.vue
@@ -0,0 +1,25 @@
+<template>
+  <Container>
+    <header>
+      <h1>{{ category.name }}</h1>
+    </header>
+    <ArticleList :articles="articles" />
+  </Container>
+</template>
+
+<script>
+import Container from './common/Container'
+import ArticleList from './common/ArticleList'
+
+export default {
+  name: 'CategoryArticleList',
+  props: {
+    category: { type: Object, required: true },
+    articles: { type: Array, required: true }
+  },
+  components: {
+    Container,
+    ArticleList
+  }
+}
+</script>
