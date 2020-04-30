24ca24ae6bf31408617e5fbf4ddd06cef0aa3bcb
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-04-30 19:10:40 +0200

    Create ArticleDetails component

--- component/common/ArticleCard.vue
+++ component/common/ArticleCard.vue
@@ -1,30 +1,7 @@
 <template>
   <article class="article-card">
     <div class="article-card__border" />
-    <div class="article-card__content">
-      <header class="content__header">
-        <p class="header__info" v-if="article.totalVisitors">
-          <EyeSVG />
-          {{ article.totalVisitors }}
-          <span v-if="article.todayVisitors">
-            (+{{ article.todayVisitors }})
-          </span>
-        </p>
-        <p class="header__info" v-if="article.readingTime">
-          <ClockSVG />
-          {{ article.readingTime }} min
-        </p>
-        <p class="header__info" v-if="article.totalComments">
-          <ReplySVG />
-          {{ article.totalComments }}
-          <span v-if="article.todayComments">
-            (+{{ article.todayComments }})
-          </span>
-        </p>
-      </header>
-      <h1>{{ article.title }}</h1>
-      <label>{{ article.description }}</label>
-    </div>
+    <ArticleDetails :article="article" class="article-card__content" />
     <div class="article-card__hover" :src="article.image.photoUrl">
       <img :src="article.image.photoUrl" />
       <Button :onClick="redirectToArticle" className="hover__button">
@@ -36,17 +13,13 @@
 
 <script>
 import Button from '../generic/Button'
-import EyeSVG from '@asset/svg/eye.svg'
-import ClockSVG from '@asset/svg/clock.svg'
-import ReplySVG from '@asset/svg/reply.svg'
+import ArticleDetails from './ArticleDetails'
 
 export default {
-  name: 'ArticleCard',
+  name: 'common-article-card',
   components: {
     Button,
-    EyeSVG,
-    ClockSVG,
-    ReplySVG
+    ArticleDetails
   },
   props: {
     article: { type: Object, required: true }
