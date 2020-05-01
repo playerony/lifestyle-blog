0077cfe50659887309c91241860a9b34881aae72
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-05-01 21:00:47 +0200

    Create tests for the menu item component

---
+++ client/component/common/ArticleList/__tests__/MenuItem.spec.js
@@ -0,0 +1,43 @@
+import { shallowMount } from '@vue/test-utils'
+
+import MenuItem from '../MenuItem'
+
+const onClickMock = jest.fn()
+
+const setUp = () =>
+  shallowMount(MenuItem, {
+    propsData: { menuOption: MENU_OPTION_MOCK, onClick: onClickMock }
+  })
+
+describe('MenuItem Component', () => {
+  beforeAll(() => {
+    console.error = jest.fn()
+  })
+
+  it('should render', () => {
+    const wrapper = setUp()
+
+    expect(wrapper.exists()).toBeTruthy()
+  })
+
+  it('is a Vue instance', () => {
+    const wrapper = setUp()
+
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('should render menu option with proper label', () => {
+    const wrapper = setUp()
+
+    expect(wrapper.find('label').text()).toEqual('label')
+  })
+
+  it('should handle click event', () => {
+    const wrapper = setUp()
+
+    wrapper.trigger('click')
+    expect(onClickMock).toHaveBeenCalledWith('sortingBy')
+  })
+})
+
+const MENU_OPTION_MOCK = { label: 'label', sortingBy: 'sortingBy' }
