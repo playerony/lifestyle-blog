02311254a9fd6f9a8cf81cb2b3bf14874d3bd095
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-05-01 19:27:43 +0200

    Cleanup article list component

---
+++ client/component/common/ArticleList/MenuItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <label class="menu__item" @click="handleClick">
+    {{ menuOption.label }}
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'common-menu-item',
+  props: {
+    onClick: { type: Function, required: true },
+    menuOption: { type: Object, required: true }
+  },
+  computed: {
+    handleClick() {
+      this.onClick(this.menuOption.sortingBy)
+    }
+  }
+}
+</script>
