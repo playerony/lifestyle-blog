fe06a21c83ed61b14a10464328baf9f2f4ca0f94
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-05-01 19:27:43 +0200

    Cleanup article list component

---
+++ component/common/ArticleList/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="article-list">
+    <ul v-if="displayMenu" class="article-list__menu">
+      <MenuItem
+        :onClick="setSortingBy"
+        :key="menuOption.label"
+        :menuOption="menuOption"
+        v-for="menuOption in menuOptions"
+        :class="selectedItemStyle(menuOption.sortingBy)"
+      />
+    </ul>
+    <transition-group
+      tag="div"
+      name="list-transition"
+      class="article-list__content"
+    >
+      <ArticleCard
+        :article="article"
+        :key="article.articleId"
+        v-for="article in getArticleList"
+      />
+    </transition-group>
+    <Tooltip
+      title="Show more"
+      :onClick="showMoreClick"
+      v-if="shouldRenderShowMoreButton"
+      class="article-list__show-more-icon"
+    >
+      <DynamicIcon icon="left-arrow" />
+    </Tooltip>
+  </div>
+</template>
+
+<script>
+import Tooltip from '../Tooltip'
+import MenuItem from './MenuItem'
+import ArticleCard from '../ArticleCard'
+import DynamicIcon from '../DynamicIcon'
+
+import sortArticleList from '@utility/sortArticleList'
+
+import menuOptions from './menuOptions'
+
+export default {
+  name: 'common-article-list',
+  props: {
+    articles: { type: Array, required: true },
+    displayMenu: { type: Boolean, required: false, default: true }
+  },
+  data() {
+    return {
+      menuOptions,
+      pageSize: 2,
+      limitItems: 4,
+      sortingBy: 'latest'
+    }
+  },
+  components: {
+    Tooltip,
+    MenuItem,
+    ArticleCard,
+    DynamicIcon
+  },
+  computed: {
+    getArticleList() {
+      return sortArticleList(this.articles, this.sortingBy).slice(
+        0,
+        this.limitItems
+      )
+    },
+    shouldRenderShowMoreButton() {
+      return this.articles.length - this.limitItems >= 1
+    },
+    showMoreClick() {
+      this.limitItems = this.limitItems + this.pageSize
+    }
+  },
+  methods: {
+    selectedItemStyle(name) {
+      if (name === this.sortingBy) {
+        return 'menu__item--selected'
+      }
+
+      return ''
+    },
+    setSortingBy(sortingBy) {
+      this.sortingBy = sortingBy
+    }
+  }
+}
+</script>
