3d96e138ebdb7076ab9ded260aaa892d8ae061fe
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-05-03 19:30:21 +0200

    Create tests for the home component

--- client/component/Home/__tests__/Home.spec.js
+++ client/component/Home/__tests__/Home.spec.js
@@ -19,6 +19,102 @@ describe('Home Component', () => {
 
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
+
+  it('should render Billboard component', () => {
+    const wrapper = setUp()
+
+    expect(wrapper.find('billboard-stub').exists()).toBeTruthy()
+  })
+
+  it('should render PinnedArticlesSwiper component', () => {
+    const wrapper = setUp()
+
+    expect(wrapper.find('pinnedarticlesswiper-stub').exists()).toBeTruthy()
+  })
+
+  it('should render Claim component', () => {
+    const wrapper = setUp()
+
+    expect(wrapper.find('claim-stub').exists()).toBeTruthy()
+  })
+
+  it('should render ArticleList component', () => {
+    const wrapper = setUp()
+
+    expect(wrapper.find('articlelist-stub').exists()).toBeTruthy()
+  })
+
+  it('should return proper last added article', () => {
+    const wrapper = setUp()
+
+    expect(wrapper.vm.getLastArticle()).toEqual({
+      likes: 1,
+      title: 'title',
+      readingTime: 8,
+      todayVisitors: 2,
+      totalVisitors: 6,
+      todayComments: 1,
+      totalComments: 10,
+      description: 'description',
+      image: { photoUrl: 'photoUrl' },
+      categoryList: [{ categoryId: 1 }]
+    })
+  })
+
+  it('should return proper article list records', () => {
+    const wrapper = setUp()
+
+    expect(wrapper.vm.getArticleList()).toEqual([
+      {
+        likes: 1,
+        title: 'title',
+        readingTime: 8,
+        todayVisitors: 2,
+        totalVisitors: 6,
+        todayComments: 1,
+        totalComments: 10,
+        description: 'description',
+        image: { photoUrl: 'photoUrl' },
+        categoryList: [{ categoryId: 1 }]
+      },
+      {
+        likes: 1,
+        title: 'title2',
+        readingTime: 8,
+        todayVisitors: 2,
+        totalVisitors: 6,
+        todayComments: 1,
+        totalComments: 10,
+        description: 'description2',
+        image: { photoUrl: 'photoUrl2' },
+        categoryList: [{ categoryId: 2 }]
+      },
+      {
+        likes: 1,
+        title: 'title4',
+        readingTime: 8,
+        todayVisitors: 2,
+        totalVisitors: 6,
+        todayComments: 1,
+        totalComments: 10,
+        description: 'description4',
+        image: { photoUrl: 'photoUrl4' },
+        categoryList: [{ categoryId: 1 }]
+      },
+      {
+        likes: 1,
+        title: 'title5',
+        readingTime: 8,
+        todayVisitors: 2,
+        totalVisitors: 6,
+        todayComments: 1,
+        totalComments: 10,
+        description: 'description5',
+        image: { photoUrl: 'photoUrl5' },
+        categoryList: [{ categoryId: 2 }]
+      }
+    ])
+  })
 })
 
 const ARTICLES_MOCK = [
@@ -31,7 +127,8 @@ const ARTICLES_MOCK = [
     todayComments: 1,
     totalComments: 10,
     description: 'description',
-    image: { photoUrl: 'photoUrl' }
+    image: { photoUrl: 'photoUrl' },
+    categoryList: [{ categoryId: 1 }]
   },
   {
     likes: 1,
@@ -42,7 +139,8 @@ const ARTICLES_MOCK = [
     todayComments: 1,
     totalComments: 10,
     description: 'description2',
-    image: { photoUrl: 'photoUrl2' }
+    image: { photoUrl: 'photoUrl2' },
+    categoryList: [{ categoryId: 2 }]
   },
   {
     likes: 1,
@@ -53,7 +151,8 @@ const ARTICLES_MOCK = [
     todayComments: 1,
     totalComments: 10,
     description: 'description3',
-    image: { photoUrl: 'photoUrl3' }
+    image: { photoUrl: 'photoUrl3' },
+    categoryList: [{ categoryId: 3 }]
   },
   {
     likes: 1,
@@ -64,7 +163,8 @@ const ARTICLES_MOCK = [
     todayComments: 1,
     totalComments: 10,
     description: 'description4',
-    image: { photoUrl: 'photoUrl4' }
+    image: { photoUrl: 'photoUrl4' },
+    categoryList: [{ categoryId: 1 }]
   },
   {
     likes: 1,
@@ -75,7 +175,8 @@ const ARTICLES_MOCK = [
     todayComments: 1,
     totalComments: 10,
     description: 'description5',
-    image: { photoUrl: 'photoUrl5' }
+    image: { photoUrl: 'photoUrl5' },
+    categoryList: [{ categoryId: 2 }]
   },
   {
     likes: 1,
@@ -86,6 +187,7 @@ const ARTICLES_MOCK = [
     todayComments: 1,
     totalComments: 10,
     description: 'description6',
-    image: { photoUrl: 'photoUrl6' }
+    image: { photoUrl: 'photoUrl6' },
+    categoryList: [{ categoryId: 3 }]
   }
 ]
