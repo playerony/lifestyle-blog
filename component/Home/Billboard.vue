62b061eb0e36258d8b0fbba650a494ca213d3ec4
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-05-03 11:47:36 +0200

    Cleanup billboard component

--- component/Home/Billboard.vue
+++ component/Home/Billboard.vue
@@ -2,28 +2,7 @@
   <div class="billboard">
     <div class="billboard__border" />
     <section class="billboard__content">
-      <header class="content__header">
-        <p class="header__info" v-if="article.totalVisitors">
-          <EyeSVG />
-          {{ article.totalVisitors }}
-          <span v-if="article.todayVisitors">
-            (+{{ article.todayVisitors }})
-          </span>
-        </p>
-        <p class="header__info" v-if="article.readingTime">
-          <ClockSVG />
-          {{ article.readingTime }} min
-        </p>
-        <p class="header__info" v-if="article.totalComments">
-          <ReplySVG />
-          {{ article.totalComments }}
-          <span v-if="article.todayComments">
-            (+{{ article.todayComments }})
-          </span>
-        </p>
-      </header>
-      <h1>{{ article.title }}</h1>
-      <label class="content__description">{{ article.description }}</label>
+      <ArticleDetails :article="article" />
       <Button :onClick="redirectToArticle" className="content__button">
         Read now
       </Button>
@@ -34,20 +13,16 @@
 
 <script>
 import Button from '../generic/Button'
-import EyeSVG from '@asset/svg/eye.svg'
-import ClockSVG from '@asset/svg/clock.svg'
-import ReplySVG from '@asset/svg/reply.svg'
+import ArticleDetails from '../common/ArticleDetails'
 
 export default {
-  name: 'Billboard',
+  name: 'home-billboard',
   props: {
     article: { type: Object, required: true }
   },
   components: {
     Button,
-    EyeSVG,
-    ClockSVG,
-    ReplySVG
+    ArticleDetails
   },
   methods: {
     redirectToArticle() {
