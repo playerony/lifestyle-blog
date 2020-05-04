aabb88112a7811288f31fe965855434b57aa552e
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-05-04 15:53:24 +0200

    ArticleList - fix bug with menu

--- component/common/ArticleList/menuOptions.js
+++ component/common/ArticleList/menuOptions.js
@@ -1,6 +1,6 @@
 export default [
   { label: 'Latest', sortingBy: 'latest' },
   { label: 'Top Rated', sortingBy: 'top-rated' },
-  { label: 'Most Viewed', sortingBy: 'top-rated' },
+  { label: 'Most Viewed', sortingBy: 'most-viewed' },
   { label: 'Most Commented', sortingBy: 'most-commented' }
 ]
--- page/ArticleList.vue
+++ page/ArticleList.vue
@@ -16,6 +16,7 @@ import CategoryArticleList from '@component/CategoryArticleList'
 import articleListQuery from '@graphql/query/articleList'
 import visitorListQuery from '@graphql/query/visitorList'
 import categoryListQuery from '@graphql/query/categoryList'
+import metricCommentList from '@graphql/query/metricCommentList'
 
 import articleListTransformator from '@transformator/articleListTransformator'
 
@@ -25,6 +26,7 @@ export default {
     return {
       articleList: [],
       visitorList: [],
+      commentList: [],
       categoryList: [],
       categoryId: Number(this.$route.params.categoryId)
     }
@@ -36,14 +38,16 @@ export default {
   apollo: {
     articleList: articleListQuery,
     visitorList: visitorListQuery,
-    categoryList: categoryListQuery
+    categoryList: categoryListQuery,
+    commentList: metricCommentList
   },
   methods: {
     isLoading() {
       return (
         this.$apollo.queries.articleList.loading ||
         this.$apollo.queries.visitorList.loading ||
-        this.$apollo.queries.categoryList.loading
+        this.$apollo.queries.categoryList.loading ||
+        this.$apollo.queries.commentList.loading
       )
     },
     getCategory() {
@@ -57,6 +61,8 @@ export default {
           element => element.categoryId === this.categoryId
         )
       )
+
+      console.warn(categoryArticles, this.visitorList, this.commentList)
 
       return articleListTransformator(
         categoryArticles,
