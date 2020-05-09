44a1ce3784545efba10204c4396f533d35c190aa
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-05-09 11:13:07 +0200

    Create tests for the findAllByCategoryId method

--- server/service/__tests__/ArticleService.test.ts
+++ server/service/__tests__/ArticleService.test.ts
@@ -1,11 +1,26 @@
+import Substitute, { Arg } from '@fluffy-spoon/substitute'
+
+import CategoryService from '@service/CategoryService'
+
 import { ArticleType } from '@type/Article'
+import { CategoryType } from '@type/Category'
+
+const _CategoryService = Substitute.for<CategoryService>()
 
 const bulkCreateMock = jest.fn()
 const scopeCallMock = jest.fn()
 const findArticleMock = jest.fn().mockImplementation(() => ARTICLE_MOCK)
+const findAllArticlesMock = jest.fn().mockImplementation(whereOptions => {
+  const onlyPublic = whereOptions.where.isPublic
+
+  return onlyPublic !== undefined
+    ? [ARTICLE_MOCK].filter(({ isPublic }) => isPublic === onlyPublic)
+    : [ARTICLE_MOCK]
+})
 
 const setupSequelizeMock = (
-  findArticle: Function = findArticleMock
+  findArticle: Function = findArticleMock,
+  findAllArticles: Function = findAllArticlesMock
 ): typeof jest =>
   jest.doMock('sequelize', () => {
     const { dataTypes: DataTypes } = require('sequelize-test-helpers')
@@ -20,16 +35,10 @@ const setupSequelizeMock = (
       public static afterCreate = jest.fn()
       public static afterUpdate = jest.fn()
       public static belongsToMany = jest.fn()
+      public static findAll = findAllArticles
       public static bulkCreate = bulkCreateMock
       public static scope = scopeCallMock.mockImplementation(() => Model)
       public static create = jest.fn().mockImplementation(() => ARTICLE_MOCK)
-      public static findAll = jest.fn().mockImplementation(whereOptions => {
-        const onlyPublic = whereOptions.where.isPublic
-
-        return onlyPublic !== undefined
-          ? [ARTICLE_MOCK].filter(({ isPublic }) => isPublic === onlyPublic)
-          : [ARTICLE_MOCK]
-      })
     }
 
     return {
@@ -42,7 +51,7 @@ const setupSequelizeMock = (
 const setUp = () => {
   const ArticleService = require('../ArticleService').default
 
-  return new ArticleService()
+  return new ArticleService(_CategoryService)
 }
 
 describe('Article Service', () => {
@@ -160,6 +169,56 @@ describe('Article Service', () => {
       expect(result).toEqual([])
     })
   })
+
+  describe('findAllByCategoryId Method', () => {
+    beforeAll(() => {
+      _CategoryService
+        .findById(Arg.any())
+        .mimicks(
+          async categoryId =>
+            CATEGORY_LIST_MOCK.find(
+              category => category.categoryId === categoryId
+            ) || null
+        )
+
+      setupSequelizeMock(
+        findArticleMock,
+        jest
+          .fn()
+          .mockImplementation(({ include }) =>
+            [ARTICLE_MOCK].filter(article =>
+              article.categoryList?.find(
+                category => category.categoryId === include[0].where.categoryId
+              )
+            )
+          )
+      )
+    })
+
+    it('should throw an error if category does not exist', async () => {
+      const articleService = setUp()
+
+      try {
+        await articleService.findAllByCategoryId(3)
+      } catch (e) {
+        expect(e.message).toEqual('{"categoryId":["No such article found."]}')
+      }
+    })
+
+    it('should return found articles', async () => {
+      const articleService = setUp()
+
+      const result = await articleService.findAllByCategoryId(1)
+      expect(result).toEqual([ARTICLE_MOCK])
+    })
+
+    it('should return empty array if there was not matched articles to provided categoryId', async () => {
+      const articleService = setUp()
+
+      const result = await articleService.findAllByCategoryId(2)
+      expect(result).toEqual([])
+    })
+  })
 })
 
 const ARTICLE_MOCK: ArticleType = {
@@ -171,8 +230,19 @@ const ARTICLE_MOCK: ArticleType = {
   content: '123',
   isPublic: false,
   readingTime: 0,
-  categoryList: [],
+  categoryList: [{ categoryId: 1 }],
   description: '123',
   createdAt: new Date('2019'),
   updatedAt: new Date('2019')
 }
+
+const CATEGORY_LIST_MOCK: CategoryType[] = [
+  {
+    categoryId: 1,
+    name: 'Name 1'
+  },
+  {
+    categoryId: 2,
+    name: 'Name 2'
+  }
+]
