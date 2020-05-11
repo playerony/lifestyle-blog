caa041701371d51cec01dffe7e7bfb6af1b2f813
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-05-11 16:09:41 +0200

    Cleanup Claim component

--- component/Home/Claim.vue
+++ component/Home/Claim.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="claim">
-    <img class="claim__image" src="https://i.imgur.com/L9AypnK.png" />
+    <img class="claim__image" src="https://i.imgur.com/EyME0a9.png" />
     <h1 class="claim__text">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      <span class="claim__rainbow">Aenaan condimentum</span> finibus nunc, sit
-      amet luctus magna placerat nec.
+      My friend who designed it forced me to write here something. So with this
+      blog, I wanna share my experience with
+      <span class="claim__rainbow">meditation</span>,
+      <span class="claim__rainbow">habits</span> and share things that changed
+      totally my life. It is writing down my knowledge in order to systematize
+      it and <span class="claim__rainbow">exchange knowledge</span>.
     </h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Claim'
+  name: 'home-claim'
 }
 </script>
