94c83915390c02ca5a6e4d1859fe4800bf8a99a8
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2020-05-13 20:24:21 +0200

    Improve article sidebar styling + add tooltips

--- component/Article/Sidebar.vue
+++ component/Article/Sidebar.vue
@@ -2,19 +2,21 @@
   <div class="side-navbar">
     <div @click="goBack" class="side-navbar__back-button" />
     <div class="side-navbar__category-list">
-      <div
+      <Tooltip
+        :title="category.name"
         :key="category.categoryId"
         class="category-list__icon"
         v-for="category in categories"
         @click="redirectToCategory(category.categoryId)"
       >
         <DynamicIcon :icon="category.name.toLowerCase()" />
-      </div>
+      </Tooltip>
     </div>
   </div>
 </template>
 
 <script>
+import Tooltip from '../common/Tooltip'
 import DynamicIcon from '../common/DynamicIcon'
 
 export default {
@@ -23,6 +25,7 @@ export default {
     categories: { type: Array, required: true }
   },
   components: {
+    Tooltip,
     DynamicIcon
   },
   methods: {
